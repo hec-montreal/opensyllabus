602f4bab1ef72f86e9872432b5b630df132907f8
Denis Balazuc <denis.balazuc@crim.ca>
2009-09-03 17:12:17 +0000

    Fix NullPointerException(s) from tests that are using mocks created through OsylMockery:
    The getExpectations() method was called in the constructor, which prevented the child classes to provide valid expectations using child's class members (as they are not yet initialized when the call to getExpectations() happens). The fix is to lazily call getExpectations() when the underlying mock is requested, and not sooner.

--- osyl-common/osyl-common-impl/impl/src/test/org/sakaiquebec/opensyllabus/common/impl/AbstractMock.java
+++ osyl-common/osyl-common-impl/impl/src/test/org/sakaiquebec/opensyllabus/common/impl/AbstractMock.java
@@ -8,18 +8,32 @@ abstract class AbstractMock<T> {
 	private Mockery mockery;
 	private T mock;
 	
+	//Meh...bad hack to avoid construction problems with getExpectation()
+	//when the later needs some class initialized fields
+	private boolean expectationSet;
+	
 	protected AbstractMock(Mockery mockery, Class<T> mocked) {
 		super();
+		this.expectationSet = false;
 		this.mockery = mockery;
-		this.mock = mockery.mock(mocked);
-		this.mockery.checking(getExpectations(mock));
+		this.mock = mockery.mock(mocked);		
 	}
 	
+	/** Returns all the Expectations for this Mock. 
+	 * 	The method is lazily called when the actual mock
+	 * 	is requested (through getMock()).
+	 * @param mock
+	 * @return
+	 */
 	protected Expectations getExpectations(T mock) {
 		return new Expectations();
 	}	
 	
 	protected final T getMock() {
+		if (!expectationSet) {
+			this.mockery.checking(getExpectations(mock));
+			expectationSet = true;
+		}
 		return this.mock;
 	}
 }
