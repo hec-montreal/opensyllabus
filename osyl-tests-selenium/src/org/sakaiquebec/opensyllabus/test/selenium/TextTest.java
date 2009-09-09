65d97fdfdfe7e7b57023297b30788f4e700a9bfa
Remi Saias <remi.saias@hec.ca>
2009-09-09 01:31:49 +0000

    Code reformating.

--- osyl-tests-selenium/src/org/sakaiquebec/opensyllabus/test/selenium/TextTest.java
+++ osyl-tests-selenium/src/org/sakaiquebec/opensyllabus/test/selenium/TextTest.java
@@ -31,18 +31,18 @@ import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStor
  * delete the previous test site if applicable, creates a new one, load it
  * (OpenSyllabus is the first and only page), enters in the first lecture, click
  * Text in the Add menu, type in some text, click OK, check the text is here,
- * click save, log out.<br/><br/>
- *
+ * click save, log out.<br/>
+ * <br/>
  * In a previous version, the test also deleted the site at the end. This could
  * be reactivated later, once we are confident enough that everything really
  * happens as it should!
- *
+ * 
  * @author <a href="mailto:remi.saias@hec.ca">Remi Saias</a>
  */
 public class TextTest extends AbstractOSYLTest {
 
     @Test(groups = "OSYL-Suite", description = "OSYL test: add a text resource, edit it and save the changes")
-    @Parameters({"webSite"})
+    @Parameters( { "webSite" })
     public void testAddText(String webSite) throws Exception {
 	// We log in
 	logInAsAdmin(webSite);
@@ -59,29 +59,33 @@ public class TextTest extends AbstractOSYLTest {
 	if (undefRubricVisible) {
 	    log("Rubric \"Undefined\" is already showing");
 	}
-	
+
 	// Click menu "Add..."
 	session().click("gwt-uid-5");
 	// Click 1st item in menu which is "Text" in this context (don't use
 	// gwt-uid because it's dynamically generated)
-	session().click("//div[@class=\"gwt-MenuBar gwt-MenuBar-vertical\"]/table/tbody/tr[1]/td");
-	
+	session().click("//div[@class=\"gwt-MenuBar gwt-MenuBar-vertical\"]" +
+			"/table/tbody/tr[1]/td");
+
 	// We check that our new text was added (this has no effect if there
 	// was already an Undef Rubric item
 	verifyTrue(session().isTextPresent("Non défini"));
-	
+
 	// open text resource editor
-	session().click("//tr[3]/td/table/tbody/tr[2]/td/div/table[2]/tbody/tr/td[1]/button");
-	
+	session().click("//tr[3]/td/table/tbody/tr[2]/td/div/table[2]/tbody" +
+			"/tr/td[1]/button");
+
 	// Change rubric
 	session().select("listBoxFormElement", "label=Description");
 	session().click("//option[@value='description']");
-	
+
 	// type text
 	session().selectFrame("//iframe[@class=\"Osyl-UnitView-TextArea\"]");
-	session().type("//html/body", "this is a text resource typed by "
-		+ "selenium, hope it works and you see it. Added on"
-		+ timeStamp());
+	session().type(
+		"//html/body",
+		"this is a text resource typed by "
+			+ "selenium, hope it works and you see it. Added on"
+			+ timeStamp());
 	session().selectFrame("relative=parent");
 	// close editor
 	session().click("//td/table/tbody/tr/td[1]/button");
@@ -90,8 +94,8 @@ public class TextTest extends AbstractOSYLTest {
 	log("OK: Text resource renamed");
 
 	// We should be back to the initial state
-	verifyTrue(session().isTextPresent("Non défini") == undefRubricVisible);
-	
+	verifyTrue(session().isTextPresent("Non défini") ==undefRubricVisible);
+
 	// Save changes
 	saveCourseOutline();
 	log("OK: Course outline saved");
