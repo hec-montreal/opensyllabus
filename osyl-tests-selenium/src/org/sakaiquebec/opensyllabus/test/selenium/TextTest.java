959256d8bec9d20c5c6d5380d3e154d53dc7e042
Remi Saias <remi.saias@hec.ca>
2009-09-09 01:10:26 +0000

    Added one subtest: change the rubric to "Description".

--- osyl-tests-selenium/src/org/sakaiquebec/opensyllabus/test/selenium/TextTest.java
+++ osyl-tests-selenium/src/org/sakaiquebec/opensyllabus/test/selenium/TextTest.java
@@ -41,44 +41,64 @@ import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStor
  */
 public class TextTest extends AbstractOSYLTest {
 
-	@Test(groups = "OSYL-Suite", description = "OSYL test: add a text resource, edit it and save the changes")
-	@Parameters({"webSite"})
-	public void testAddText(String webSite) throws Exception {
-		// We log in
-		logInAsAdmin(webSite);
-		// We delete the test site if it exists. If it does not, we don't fail!
-		deleteTestSite(false);
-		// We create a brand new one to ensure a constant "playground"!
-		createTestSite();
-		goToSite();
-		waitForOSYL();
-		enterFirstLecture();
+    @Test(groups = "OSYL-Suite", description = "OSYL test: add a text resource, edit it and save the changes")
+    @Parameters({"webSite"})
+    public void testAddText(String webSite) throws Exception {
+	// We log in
+	logInAsAdmin(webSite);
+	// We delete the test site if it exists. If it does not, we don't fail!
+	deleteTestSite(false);
+	// We create a brand new one to ensure a constant "playground"!
+	createTestSite();
+	goToSite();
+	waitForOSYL();
+	enterFirstLecture();
 
-		// Click menu "Add..."
-		session().click("gwt-uid-5");
-		// Click 1st item in menu which is "Text" in this context (don't use
-		// gwt-uid because it's dynamically generated)
-		session().click("//div[@class=\"gwt-MenuBar gwt-MenuBar-vertical\"]/table/tbody/tr[1]/td");
-		// open text resource editor
-		session().click("//tr[3]/td/table/tbody/tr[2]/td/div/table[2]/tbody/tr/td[1]/button");
-		// type text
-		session().selectFrame("//iframe[@class=\"Osyl-UnitView-TextArea\"]");
-		session().type("//html/body", "this is a text resource typed by "
-					   + "selenium, hope it works and you see it. Added on"
-					   + timeStamp());
-		session().selectFrame("relative=parent");
-		// close editor
-		session().click("//td/table/tbody/tr/td[1]/button");
-		// check if text is visible
-		verifyTrue(session().isTextPresent("text resource typed by selenium"));
-		log("OK: Text resource renamed");
-		// Save changes
-		saveCourseOutline();
-		log("OK: Course outline saved");
+	// We first check if we already have an undefined rubric
+	boolean undefRubricVisible = session().isTextPresent("Non défini");
+	if (undefRubricVisible) {
+	    log("Rubric \"Undefined\" is already showing");
+	}
+	
+	// Click menu "Add..."
+	session().click("gwt-uid-5");
+	// Click 1st item in menu which is "Text" in this context (don't use
+	// gwt-uid because it's dynamically generated)
+	session().click("//div[@class=\"gwt-MenuBar gwt-MenuBar-vertical\"]/table/tbody/tr[1]/td");
+	
+	// We check that our new text was added (this has no effect if there
+	// was already an Undef Rubric item
+	verifyTrue(session().isTextPresent("Non défini"));
+	
+	// open text resource editor
+	session().click("//tr[3]/td/table/tbody/tr[2]/td/div/table[2]/tbody/tr/td[1]/button");
+	
+	// Change rubric
+	session().select("listBoxFormElement", "label=Description");
+	session().click("//option[@value='description']");
+	
+	// type text
+	session().selectFrame("//iframe[@class=\"Osyl-UnitView-TextArea\"]");
+	session().type("//html/body", "this is a text resource typed by "
+		+ "selenium, hope it works and you see it. Added on"
+		+ timeStamp());
+	session().selectFrame("relative=parent");
+	// close editor
+	session().click("//td/table/tbody/tr/td[1]/button");
+	// check if text is visible
+	verifyTrue(session().isTextPresent("text resource typed by selenium"));
+	log("OK: Text resource renamed");
 
-		session().selectFrame("relative=parent");
-		// See class comment above
-		// deleteTestSite();
-		logOut();
-	} // testAddText
+	// We should be back to the initial state
+	verifyTrue(session().isTextPresent("Non défini") == undefRubricVisible);
+	
+	// Save changes
+	saveCourseOutline();
+	log("OK: Course outline saved");
+
+	session().selectFrame("relative=parent");
+	// See class comment above
+	// deleteTestSite();
+	logOut();
+    } // testAddText
 }
