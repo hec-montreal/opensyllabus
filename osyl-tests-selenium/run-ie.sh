7819e7f5dbb9780153d00544b5940d69a7751d72
Remi Saias <remi.saias@hec.ca>
2009-09-22 14:16:33 +0000

    Added config and script files for running with MSIE. Minor corrections to Firefox files.

---
+++ osyl-tests-selenium/run-ie.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -classpath classes:lib/testng-5.9-jdk15.jar:lib/selenium-java-client-driver.jar:lib/selenium-grid-tools-1.0.3.jar:lib/junit-4.6.jar:lib/commons-logging-1.0.4.jar org.testng.TestNG OSYLTestSuite-ie.xml
