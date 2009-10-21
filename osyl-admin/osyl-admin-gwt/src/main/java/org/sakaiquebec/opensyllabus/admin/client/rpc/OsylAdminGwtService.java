a12b5f9b804cd0cc0a2b22bcf8c29bb7da3bf505
Mathieu Cantin <mathieu.cantin@hec.ca>
2009-10-21 20:46:48 +0000

    On déplace le rpc du sakai-tool au projet gwt

---
+++ osyl-admin/osyl-admin-gwt/src/main/java/org/sakaiquebec/opensyllabus/admin/client/rpc/OsylAdminGwtService.java
@@ -0,0 +1,82 @@
+/******************************************************************************
+ * $Id: $
+ ******************************************************************************
+ *
+ * Copyright (c) 2008 The Sakai Foundation, The Sakai Quebec Team.
+ *
+ * Licensed under the Educational Community License, Version 1.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.sakaiquebec.opensyllabus.admin.client.rpc;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+
+/**
+ * OsylAdminGwtService defines the RPC (Remote Procedure Call) interface
+ * between the GWT client application and the Sakai server.
+ * 
+ * @author <a href="mailto:mame-awa.diop@hec.ca">Mame Awa Diop</a>
+ * @version $Id: $
+ */
+public interface OsylAdminGwtService extends RemoteService{
+
+    /**
+     * Creates users from data extracts located in specified directory.
+     * 
+     * @param fileDirectory
+     */
+    public void createUsers(String fileDirectory);
+
+    /**
+     * Returns all defined roles of the realm template
+     * 
+     * @return a Set containing the IDs of the roles
+     */
+    public Set<String> getTemplateRoles();
+    
+    /**
+     * Returns the functions with the lists of allowed roles 
+     * of the realm template
+     * 
+     * @return a HashMap containing the function as key and the
+     * 		list with the allowed roles as value
+     */
+    public HashMap<String, List<String>> getTemplateFunctionsWithAllowedRoles();
+    
+    /**
+     * Gives all roles in the given list the permission for this
+     * function. Roles not included in the list lose the
+     * permission.
+     * 
+     * @param function the name of the function to update
+     * @param allowedRoles names of the roles which have permission 
+     * 		for this function
+     */
+    public void updateTemplateFunction(String function, Set<String> allowedRoles);
+    
+    /**
+     * Gives all roles in the given list the permission for the
+     * functions which are the key values of the HashMap. 
+     * Roles not included in the list lose the permission.
+     * 
+     * @param functions the updated functions keys and the allowed
+     * 		roles as values
+     */
+    public void updateTemplateFunctions(HashMap<String, Set<String>> functions);
+ }
