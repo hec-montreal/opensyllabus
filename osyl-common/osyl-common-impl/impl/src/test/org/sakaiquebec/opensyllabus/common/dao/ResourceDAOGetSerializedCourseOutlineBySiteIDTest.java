8f206bd98311a0acf5625632cb1327e69cdca905
Laurent Danet <laurent.danet@hec.ca>
2009-10-22 12:23:56 +0000

    JUnit test fix

--- osyl-common/osyl-common-impl/impl/src/test/org/sakaiquebec/opensyllabus/common/dao/ResourceDAOGetSerializedCourseOutlineBySiteIDTest.java
+++ osyl-common/osyl-common-impl/impl/src/test/org/sakaiquebec/opensyllabus/common/dao/ResourceDAOGetSerializedCourseOutlineBySiteIDTest.java
@@ -14,39 +14,8 @@ public class ResourceDAOGetSerializedCourseOutlineBySiteIDTest extends AbstractR
 		resourceDAO.createOrUpdateCourseOutline(course);
 		
 		try {
-			resourceDAO.getSerializedCourseOutlineBySiteId(null, null);
+			resourceDAO.getSerializedCourseOutlineBySiteId(null);
 			fail("getSerializedCourseOutlineBySiteId(): expected an exception because of null parameters.");
-		}
-		catch (Exception e) {
-			//fine
-		}
-	}	
-	public void testGetNullGroupName() throws Exception {
-		ResourceDao resourceDAO = getResourceDAO();
-		
-		COSerialized course = newCourseOutline("0");
-		course.setAccess("sec");
-		resourceDAO.createOrUpdateCourseOutline(course);
-		
-		try {
-			resourceDAO.getSerializedCourseOutlineBySiteId(course.getSiteId(), null);
-			fail("getSerializedCourseOutlineBySiteId(): expected an exception because of a null group parameter.");
-		}
-		catch (Exception e) {
-			//fine
-		}
-	}	
-	
-	public void testGetNullCourseID() throws Exception {
-		final String GROUP = "UnitTest";
-		ResourceDao resourceDAO = getResourceDAO();
-		
-		COSerialized course = newCourseOutline("0");
-		course.setAccess(GROUP);
-		resourceDAO.createOrUpdateCourseOutline(course);
-		try {
-			resourceDAO.getSerializedCourseOutlineBySiteId(null, GROUP);
-			fail("getSerializedCourseOutlineBySiteId(): expected an exception because of a null course parameter.");
 		}
 		catch (Exception e) {
 			//fine
@@ -63,8 +32,7 @@ public class ResourceDAOGetSerializedCourseOutlineBySiteIDTest extends AbstractR
 		
 		COSerialized otherCourse = 
 			resourceDAO.getSerializedCourseOutlineBySiteId(
-					course.getSiteId(), 
-					course.getAccess());
+					course.getSiteId());
 		
 		assertEquals(
 				"Course retrieved using getSerializedCourseOutlineBySiteId() doesn't match the created one.",
@@ -75,7 +43,6 @@ public class ResourceDAOGetSerializedCourseOutlineBySiteIDTest extends AbstractR
 		ResourceDao resourceDAO = getResourceDAO();
 		try {
 			resourceDAO.getSerializedCourseOutlineBySiteId(
-					"" + System.currentTimeMillis(), 
 					"" + System.currentTimeMillis());
 			fail("Expected an Exception from 'getSerializedCourseOutlineBySiteId' since the course doesn't exist.");
 		}
@@ -84,39 +51,4 @@ public class ResourceDAOGetSerializedCourseOutlineBySiteIDTest extends AbstractR
 		}
 	}
 	
-	public void testGetExistingCourseWithNonExistingGroup() throws Exception {
-		final String GROUP = "UnitTest";
-		ResourceDao resourceDAO = getResourceDAO();
-		
-		COSerialized course = newCourseOutline("0");
-		course.setAccess(GROUP);
-		resourceDAO.createOrUpdateCourseOutline(course);
-		try {
-			resourceDAO.getSerializedCourseOutlineBySiteId(
-					course.getSiteId(), 
-					"" + System.currentTimeMillis());
-			fail("Expected an Exception from 'getSerializedCourseOutlineBySiteId' since the course doesn't exist.");
-		}
-		catch (Exception e) {
-			//fine
-		}
-	}
-	
-	public void testGetNonExistingCourseWithExistingGroup() throws Exception {
-		final String GROUP = "UnitTest";
-		ResourceDao resourceDAO = getResourceDAO();
-		
-		COSerialized course = newCourseOutline("0");
-		course.setAccess(GROUP);
-		resourceDAO.createOrUpdateCourseOutline(course);
-		try {
-			resourceDAO.getSerializedCourseOutlineBySiteId(
-					"" + System.currentTimeMillis(), 
-					GROUP);
-			fail("Expected an Exception from 'getSerializedCourseOutlineBySiteId' since the course doesn't exist.");
-		}
-		catch (Exception e) {
-			//fine
-		}
-	}
 }
