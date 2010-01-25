dcca36f7c5e2a5530fc8d9b3fbb33361d1560260
Remi Saias <remi.saias@hec.ca>
2010-01-25 15:22:59 +0000

    SAKAI-1206: correcting unit test for configurations

--- osyl-common/osyl-common-impl/impl/src/test/org/sakaiquebec/opensyllabus/common/impl/OsylConfigServiceInitConfigsTest.java
+++ osyl-common/osyl-common-impl/impl/src/test/org/sakaiquebec/opensyllabus/common/impl/OsylConfigServiceInitConfigsTest.java
@@ -1,19 +1,22 @@
 package org.sakaiquebec.opensyllabus.common.impl;
 
 //This test checks that initConfigs() creates the number of configs it's supposed to (2 according to the javadoc).
-public final class OsylConfigServiceInitConfigsTest extends AbstractOsylConfigServiceTest {
+public final class OsylConfigServiceInitConfigsTest extends
+	AbstractOsylConfigServiceTest {
 
-	//The OsylServiceConfig interface says:
-	//Temporary method to automatically create 2 configs (HEC - UdeM) in the corresponding table.
-	//Let's verify that...
+    // The OsylServiceConfig interface says:
+    // Temporary method to automatically create 9 configs (2 for HEC, 5 for
+    // UdeM, 2 for uVa) in the corresponding table.
+    // Let's verify that...
     public void testInitConfigs() throws Exception {
-    	//DO NOT CALL initConfigs(), the IMPL does it already through init()
-    	//(which in itself shows that initConfigs() should not be part of the interface).
-    	//We expect 6 configs as the doc says.
-    	
-    	assertEquals(
-    			"OsylServiceConfig.initConfigs() didn't created the number of " + 
-    			"COConfigSerialized that the documentation says it does.",
-    			7, getService().getConfigs().size());    	
+	// DO NOT CALL initConfigs(), the IMPL does it already through init()
+	// (which in itself shows that initConfigs() should not be part of the
+	// interface).
+	// We expect 9 configs as the doc says.
+
+	assertEquals(
+		"OsylServiceConfig.initConfigs() didn't created the number of "
+			+ "COConfigSerialized that the documentation says it does.",
+		9, getService().getConfigs().size());
     }
 }
