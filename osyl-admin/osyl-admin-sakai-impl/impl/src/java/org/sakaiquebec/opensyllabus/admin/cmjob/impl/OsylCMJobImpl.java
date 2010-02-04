bada779342dd2a6b227255a9346ad414001ceebb
Mame Awa Diop <mame-awa.diop@hec.ca>
2010-02-04 21:16:25 +0000

    Mise à jour pour Oracle

--- osyl-admin/osyl-admin-sakai-impl/impl/src/java/org/sakaiquebec/opensyllabus/admin/cmjob/impl/OsylCMJobImpl.java
+++ osyl-admin/osyl-admin-sakai-impl/impl/src/java/org/sakaiquebec/opensyllabus/admin/cmjob/impl/OsylCMJobImpl.java
@@ -230,7 +230,7 @@ public class OsylCMJobImpl implements OsylCMJob {
 					enrollmentSet = cmAdmin.createEnrollmentSet(enrollmentSetId, detailsCours
 							.getCourseTitleLong(), detailsCours
 							.getCourseTitleLong(), "", "", courseOfferingId,
-							"");
+							new HashSet());
 					enrollmentSet.setOfficialInstructors(instructors);
 					Section courseSection = cmService.getSection(courseSectionId);
 					if (courseSection != null){
