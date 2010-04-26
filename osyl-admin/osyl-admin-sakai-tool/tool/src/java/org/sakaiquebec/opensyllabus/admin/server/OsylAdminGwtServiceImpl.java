5b3418d7a5ae16f3e3db39015e36f5b2b6e80c33
Mame Awa Diop <mame-awa.diop@hec.ca>
2010-04-26 14:44:43 +0000

    Correction sur synchro

--- osyl-admin/osyl-admin-sakai-tool/tool/src/java/org/sakaiquebec/opensyllabus/admin/server/OsylAdminGwtServiceImpl.java
+++ osyl-admin/osyl-admin-sakai-tool/tool/src/java/org/sakaiquebec/opensyllabus/admin/server/OsylAdminGwtServiceImpl.java
@@ -21,11 +21,6 @@
 
 package org.sakaiquebec.opensyllabus.admin.server;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-import java.util.Map.Entry;
 
 import javax.servlet.ServletContext;
 
