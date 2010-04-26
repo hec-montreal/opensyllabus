000eaa45142da66e8391ee9aaf67b026f188618e
Mame Awa Diop <mame-awa.diop@hec.ca>
2010-04-26 14:42:24 +0000

    Correction sur synchro

--- osyl-admin/osyl-admin-gwt/src/main/java/org/sakaiquebec/opensyllabus/admin/server/OsylAdminGwtServiceImpl.java
+++ osyl-admin/osyl-admin-gwt/src/main/java/org/sakaiquebec/opensyllabus/admin/server/OsylAdminGwtServiceImpl.java
@@ -1,9 +1,6 @@
 package org.sakaiquebec.opensyllabus.admin.server;
 
 import org.sakaiquebec.opensyllabus.admin.client.rpc.OsylAdminGwtService;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
 
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
@@ -16,12 +13,5 @@ public class OsylAdminGwtServiceImpl extends RemoteServiceServlet implements Osy
 
     public void createUsers(String fileDirectory){};
      
-    public Set<String> getTemplateRoles() {return null;};
-    public HashMap<String, List<String>> getTemplateFunctionsWithAllowedRoles(){return null;};
     
-  
-    public void updateTemplateFunction(String function, Set<String> allowedRoles){};
-    
-    public void updateTemplateFunctions(HashMap<String, Set<String>> functions){};
-   
 }
