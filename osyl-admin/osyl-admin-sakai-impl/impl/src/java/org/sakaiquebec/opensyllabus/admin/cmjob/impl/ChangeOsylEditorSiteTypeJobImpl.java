9763253cf3cc274cee71f5ba1bfa39d55b0812bc
Mame Awa Diop <mame-awa.diop@hec.ca>
2010-04-29 18:11:52 +0000

    Ajustement pour les changements de type de site

--- osyl-admin/osyl-admin-sakai-impl/impl/src/java/org/sakaiquebec/opensyllabus/admin/cmjob/impl/ChangeOsylEditorSiteTypeJobImpl.java
+++ osyl-admin/osyl-admin-sakai-impl/impl/src/java/org/sakaiquebec/opensyllabus/admin/cmjob/impl/ChangeOsylEditorSiteTypeJobImpl.java
@@ -1,7 +1,11 @@
 package org.sakaiquebec.opensyllabus.admin.cmjob.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.Vector;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -19,9 +23,13 @@ import org.sakaiproject.event.cover.UsageSessionService;
 import org.sakaiproject.exception.IdUnusedException;
 import org.sakaiproject.exception.PermissionException;
 import org.sakaiproject.site.api.Site;
+import org.sakaiproject.site.api.SitePage;
 import org.sakaiproject.site.api.SiteService;
+import org.sakaiproject.site.api.ToolConfiguration;
 import org.sakaiproject.tool.api.Session;
+import org.sakaiproject.tool.api.Tool;
 import org.sakaiproject.tool.cover.SessionManager;
+import org.sakaiproject.tool.cover.ToolManager;
 import org.sakaiproject.user.api.UserDirectoryService;
 import org.sakaiproject.user.api.UserNotDefinedException;
 import org.sakaiquebec.opensyllabus.admin.cmjob.api.BibliServPretSynchronizationJob;
@@ -54,13 +62,19 @@ public class ChangeOsylEditorSiteTypeJobImpl implements Job {
 
     private final static String SITE_TYPE = "osylEditor";
 
+    private final static String[] DEFAULT_TOOLS =
+	    { "sakai.opensyllabus.tool", "sakai.assignment.grades",
+		    "sakai.resources", "sakai.siteinfo" };
+
+    private List<ToolConfiguration> siteTools = null;
+
     @SuppressWarnings("unchecked")
     public void execute(JobExecutionContext arg0) throws JobExecutionException {
 
 	loginToSakai();
 
 	allSites =
-		siteService.getSites(SiteService.SelectionType.ANY, SITE_TYPE,
+		siteService.getSites(SiteService.SelectionType.ANY, "course",
 			null, null, SiteService.SortType.NONE, null);
 
 	Site site = null;
@@ -69,9 +83,13 @@ public class ChangeOsylEditorSiteTypeJobImpl implements Job {
 
 	    site = allSites.get(i);
 	    if (site != null) {
-		site.setType(NEW_SITE_TYPE);
 		try {
+		    siteTools = getSiteTools(site);
+		    System.out.println(siteTools.size());
+		    site.setType(NEW_SITE_TYPE);
+
 		    siteService.save(site);
+		    validateTools(site);
 		} catch (IdUnusedException e) {
 		    log.error(e.getMessage());
 		} catch (PermissionException e) {
@@ -80,6 +98,66 @@ public class ChangeOsylEditorSiteTypeJobImpl implements Job {
 	    }
 	}
 	logoutFromSakai();
+    }
+
+    private List<ToolConfiguration> getSiteTools(Site site) {
+	List pages = new Vector(site.getPages());
+	List tools = new ArrayList();
+
+	for (Iterator p = pages.iterator(); p.hasNext();) {
+	    SitePage page = (SitePage) p.next();
+	    tools.addAll(page.getTools());
+	}
+
+	return tools;
+    }
+
+    private void validateTools(Site site) {
+	List<ToolConfiguration> newSiteTools = getSiteTools(site);
+
+	if ((siteTools != newSiteTools) || (newSiteTools.size() == 0)) {
+	    String[] toolsToAdd;
+	    if (siteTools.size() == 0)
+		toolsToAdd = DEFAULT_TOOLS;
+	    else {
+		toolsToAdd = new String[siteTools.size()];
+		int i = 0;
+		for (ToolConfiguration tool : siteTools) {
+		    toolsToAdd[i++] = tool.getToolId();
+		}
+	    }
+
+	    for (int i=0; i<toolsToAdd.length; i++){
+		addTool(site, toolsToAdd[i]);
+	    }
+	    try {
+		siteService.save(site);
+	    } catch (IdUnusedException e) {
+		log.info(e.getMessage());
+	    } catch (PermissionException e) {
+		log.info(e.getMessage());
+	    }
+	}
+    }
+
+    public void addTool(Site site, String toolId) {
+	SitePage page = site.addPage();
+	Tool tool = ToolManager.getTool(toolId);
+	page.setTitle(tool.getTitle());
+	page.setLayout(SitePage.LAYOUT_SINGLE_COL);
+	ToolConfiguration toolConf = page.addTool();
+	toolConf.setTool(toolId, tool);
+	toolConf.setTitle(tool.getTitle());
+	toolConf.setLayoutHints("0,0");
+
+	try {
+	    siteService.save(site);
+	} catch (IdUnusedException e) {
+	    log.error("Add tool - Unused id exception", e);
+	} catch (PermissionException e) {
+	    log.error("Add tool - Permission exception", e);
+	}
+
     }
 
     /**
