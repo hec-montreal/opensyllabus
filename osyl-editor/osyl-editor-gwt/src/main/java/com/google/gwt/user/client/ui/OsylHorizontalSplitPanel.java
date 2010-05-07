a933e76d9232b3ee1cca418f14ec5f233abe0995
Gilles-Philippe Leblanc <gilles-philippe.leblanc@umontreal.ca>
2010-05-07 13:35:01 +0000

    SAKAI-1475

--- osyl-editor/osyl-editor-gwt/src/main/java/com/google/gwt/user/client/ui/OsylHorizontalSplitPanel.java
+++ osyl-editor/osyl-editor-gwt/src/main/java/com/google/gwt/user/client/ui/OsylHorizontalSplitPanel.java
@@ -114,6 +114,7 @@ public class OsylHorizontalSplitPanel extends Composite {
 	    Element target = DOM.eventGetTarget(event);
 	    if (DOM.isOrHasChild(collapseElement, target)) {
 		collapseMouseDown = true;
+		collapseAnchor.addStyleName("down");
 		DOM.eventPreventDefault(event);
 	    }
 	    break;
@@ -136,6 +137,7 @@ public class OsylHorizontalSplitPanel extends Composite {
 		}
 		leftElementVisible = !leftElementVisible;
 		collapseMouseDown = false;
+		collapseAnchor.removeStyleName("down");
 		handler.onMouseMove(null);
 		DOM.eventPreventDefault(event);
 	    }
