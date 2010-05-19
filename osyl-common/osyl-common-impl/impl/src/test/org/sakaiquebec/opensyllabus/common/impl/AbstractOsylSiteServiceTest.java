8fb8520d2da6ccce2da970fbc82fe6696aefbdd2
Laurent Danet <laurent.danet@hec.ca>
2010-05-19 13:15:13 +0000

    SAKAI-208 : fix junit tests

--- osyl-common/osyl-common-impl/impl/src/test/org/sakaiquebec/opensyllabus/common/impl/AbstractOsylSiteServiceTest.java
+++ osyl-common/osyl-common-impl/impl/src/test/org/sakaiquebec/opensyllabus/common/impl/AbstractOsylSiteServiceTest.java
@@ -1,6 +1,10 @@
 package org.sakaiquebec.opensyllabus.common.impl;
 
+import org.jmock.Expectations;
+import org.sakaiproject.event.api.EventTrackingService;
 import org.sakaiproject.site.api.SiteService;
+import org.sakaiproject.tool.api.SessionManager;
+import org.sakaiproject.user.api.UserDirectoryService;
 import org.sakaiquebec.opensyllabus.common.api.OsylConfigService;
 import org.sakaiquebec.opensyllabus.common.api.OsylSecurityService;
 import org.sakaiquebec.opensyllabus.common.api.OsylSiteService;
@@ -9,37 +13,53 @@ import org.sakaiquebec.opensyllabus.common.dao.CORelationDao;
 import org.sakaiquebec.opensyllabus.common.dao.ResourceDao;
 
 public abstract class AbstractOsylSiteServiceTest extends AbstractServiceTest {
-    
+
     private OsylSiteService service;
-    
+
     private SiteService siteService;
-    
+
     public final OsylSiteService getService() {
-        return this.service;
+	return this.service;
     }
 
     @Override
     protected void onServiceSetUp() throws Exception {
-        this.siteService = getMockery().newSiteService();
-        OsylSiteServiceImpl serviceImpl = new OsylSiteServiceImpl();
-        
-        serviceImpl.setConfigDao(getMockery().mock(COConfigDao.class));
-        serviceImpl.setCoRelationDao(getMockery().mock(CORelationDao.class));
-        serviceImpl.setResourceDao(getMockery().mock(ResourceDao.class));
-                
-        serviceImpl.setContentHostingService(getMockery().newContentHostingService());        
-        serviceImpl.setSiteService(this.siteService);
-        serviceImpl.setToolManager(getMockery().newToolManager());
-        serviceImpl.setIdManager(getMockery().newIdManager());
-        
-        serviceImpl.setConfigService(getMockery().mock(OsylConfigService.class));
-        serviceImpl.setSecurityService(getMockery().mock(OsylSecurityService.class));
-        
-        serviceImpl.init();
-        this.service = serviceImpl;
+	this.siteService = getMockery().newSiteService();
+	OsylSiteServiceImpl serviceImpl = new OsylSiteServiceImpl();
+
+	serviceImpl.setConfigDao(getMockery().mock(COConfigDao.class));
+	serviceImpl.setCoRelationDao(getMockery().mock(CORelationDao.class));
+	serviceImpl.setResourceDao(getMockery().mock(ResourceDao.class));
+
+	serviceImpl.setContentHostingService(getMockery()
+		.newContentHostingService());
+	serviceImpl.setSiteService(this.siteService);
+	serviceImpl.setToolManager(getMockery().newToolManager());
+	serviceImpl.setIdManager(getMockery().newIdManager());
+
+	serviceImpl
+		.setConfigService(getMockery().mock(OsylConfigService.class));
+	serviceImpl.setSecurityService(getMockery().mock(
+		OsylSecurityService.class));
+	serviceImpl.setUserDirectoryService(getMockery().mock(
+		UserDirectoryService.class));
+
+	final EventTrackingService eventTrackingService =
+		getMockery().mock(EventTrackingService.class);
+	serviceImpl.setEventTrackingService(eventTrackingService);
+	getMockery().checking(new Expectations() {
+	    {
+		ignoring(eventTrackingService);
+	    }
+	});
+
+	serviceImpl.setSessionManager(getMockery().mock(SessionManager.class));
+
+	serviceImpl.init();
+	this.service = serviceImpl;
     }
-    
+
     protected final SiteService getSiteService() {
-        return this.siteService;
+	return this.siteService;
     }
 }
