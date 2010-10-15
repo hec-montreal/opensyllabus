90eca3633ea96008012cd69b4ba9f26a3b6d8a0c
Laurent Danet <laurent.danet@hec.ca>
2010-10-15 14:47:41 +0000

    SAKAI-2025 : fix tests error Add asserts to 'Associate' test.

--- osyl-tests-selenium/src/org/sakaiquebec/opensyllabus/test/selenium/AssociateToParentSite.java
+++ osyl-tests-selenium/src/org/sakaiquebec/opensyllabus/test/selenium/AssociateToParentSite.java
@@ -30,7 +30,7 @@ import org.testng.annotations.Test;
  * @version $Id: $
  */
 public class AssociateToParentSite extends AbstractOSYLTest {
-    
+
     @Test(groups = "OSYL-Suite", description = "OSYLEditor OsylManagertest. Associate to a parent site")
     @Parameters( { "webSite" })
     public void testAssociateToParentSite(String webSite) throws Exception {
@@ -54,7 +54,7 @@ public class AssociateToParentSite extends AbstractOSYLTest {
 	    goToSite(childSiteName);
 	}
 	waitForOSYL();
-	
+
 	session().selectFrame("relative=parent");
 	String parentSiteName = getCurrentTestSiteName() + "_Parent";
 	try {
@@ -64,190 +64,258 @@ public class AssociateToParentSite extends AbstractOSYLTest {
 	    goToSite(parentSiteName);
 	}
 	waitForOSYL();
-	
-	try{
-	//add content
-	openTeachingMaterialSection();
-	
-	String parentText= "this is a text resource typed by "
-	    + "selenium, hope it works and you see it. Added on "
-	    + timeStamp() + " in Firefox";
-	
-	addText(parentText,LEVEL_ATTENDEE);
-	
-	//publish
-	session().click("gwt-uid-5");
-	session().click("//button[contains(text(),'Publier')]");
-	pause();
-	
-	//attach to parent
-	session().selectFrame("relative=parent");
-	goToOsylManagerTool();
-	session().type("//input[@class='gwt-TextBox']", getCurrentTestSiteName());
-	session().mouseOver("//div[@class='Osyl-Button Osyl-Button-up']");
-	session().mouseDown("//div[@class='Osyl-Button Osyl-Button-up-hovering']");
-	session().mouseUp("//div[@class='Osyl-Button Osyl-Button-down-hovering']");
-	pause();
-	session().click("//tr[@class='OsylManager-scrollTable-row']/td/input");
-	pause();
-	session().mouseOver("//div[@class='OsylManager-action OsylManager-action-up' and ./div[contains(.,'Lier')]]");
-	session().mouseDown("//div[@class='OsylManager-action OsylManager-action-up-hovering' and ./div[contains(.,'Lier')]]");
-	session().mouseUp("//div[@class='OsylManager-action OsylManager-action-down-hovering' and ./div[contains(.,'Lier')]]");
-	pause();
-	session().select("//select[@class='OsylManager-form-element']", "label="+parentSiteName);
-	pause();
-	session().mouseOver("//div[@class='Osyl-Button Osyl-Button-up' and ./div[contains(.,'Lier')]]");
-	session().mouseDown("//div[@class='Osyl-Button Osyl-Button-up-hovering' and ./div[contains(.,'Lier')]]");
-	session().mouseUp("//div[@class='Osyl-Button Osyl-Button-down-hovering' and ./div[contains(.,'Lier')]]");
-	pause();
-	assertTrue(session().isElementPresent("//div[contains(.,'bien')]"));
-	session().mouseOver("//div[@class='Osyl-Button Osyl-Button-up' and ./div[contains(.,'Fermer')]]");
-	session().mouseDown("//div[@class='Osyl-Button Osyl-Button-up-hovering' and ./div[contains(.,'Fermer')]]");
-	session().mouseUp("//div[@class='Osyl-Button Osyl-Button-down-hovering' and ./div[contains(.,'Fermer')]]");
-	
-	//verify if text is present
-	goToCurrentSite();
-	openTeachingMaterialSection();
-	assertTrue(session().isTextPresent(parentText));
-	
-	String currentSiteText = "this is a text resource typed by "
-	    + "selenium, hope it works and you see it. Added on "
-	    + timeStamp() + " in Firefox";
-	addText(currentSiteText,LEVEL_ATTENDEE);
-	
-	//publish
-	session().click("gwt-uid-5");
-	session().click("//button[contains(text(),'Publier')]");
-	pause();
-	
-	//attach child to current site
-	session().selectFrame("relative=parent");
-	goToOsylManagerTool();
-	session().type("//input[@class='gwt-TextBox']", childSiteName);
-	session().mouseOver("//div[@class='Osyl-Button Osyl-Button-up']");
-	session().mouseDown("//div[@class='Osyl-Button Osyl-Button-up-hovering']");
-	session().mouseUp("//div[@class='Osyl-Button Osyl-Button-down-hovering']");
-	pause();
-	session().click("//tr[@class='OsylManager-scrollTable-row']/td/input");
-	pause();
-	session().mouseOver("//div[@class='OsylManager-action OsylManager-action-up' and ./div[contains(.,'Lier')]]");
-	session().mouseDown("//div[@class='OsylManager-action OsylManager-action-up-hovering' and ./div[contains(.,'Lier')]]");
-	session().mouseUp("//div[@class='OsylManager-action OsylManager-action-down-hovering' and ./div[contains(.,'Lier')]]");
-	pause();
-	session().select("//select[@class='OsylManager-form-element']", "label="+getCurrentTestSiteName());
-	pause();
-	session().mouseOver("//div[@class='Osyl-Button Osyl-Button-up' and ./div[contains(.,'Lier')]]");
-	session().mouseDown("//div[@class='Osyl-Button Osyl-Button-up-hovering' and ./div[contains(.,'Lier')]]");
-	session().mouseUp("//div[@class='Osyl-Button Osyl-Button-down-hovering' and ./div[contains(.,'Lier')]]");
-	pause();
-	assertTrue(session().isElementPresent("//div[contains(.,'bien')]"));
-	session().mouseOver("//div[@class='Osyl-Button Osyl-Button-up' and ./div[contains(.,'Fermer')]]");
-	session().mouseDown("//div[@class='Osyl-Button Osyl-Button-up-hovering' and ./div[contains(.,'Fermer')]]");
-	session().mouseUp("//div[@class='Osyl-Button Osyl-Button-down-hovering' and ./div[contains(.,'Fermer')]]");
-	
-	//verify if text is present
-	goToSite(childSiteName);
-	openTeachingMaterialSection();
-	assertTrue(session().isTextPresent(parentText));
-	assertTrue(session().isTextPresent(currentSiteText));
-	
-	//go to parent site
-	session().selectFrame("relative=parent");
-	goToSite(parentSiteName);
-	openTeachingMaterialSection();
 
-	//delete text
-	// We delete new added docuement
-	session().click("//tr/td/div/table[2]/tbody/tr/td[2]/button");
-	pause();
+	try {
+	    // add content
+	    openTeachingMaterialSection();
 
-	session()
-		.click(
-			"//tr/td[2]/div/table/tbody/tr[2]/td/table/tbody/tr/td/button");
-	pause();
-	
-	// ---------------------------------------------------------------------------//
-	// Add Document //
-	// ---------------------------------------------------------------------------//
-	String parentDocumentClickabletext = timeStamp();
-	addDocument("osyl-src[1].zip", parentDocumentClickabletext);
-	
-	//publish
-	session().click("gwt-uid-5");
-	session().click("//button[contains(text(),'Publier')]");
-	pause();
-	
-	//verify if text of parentis present in current site
-	session().selectFrame("relative=parent");
-	goToCurrentSite();
-	openTeachingMaterialSection();
-	assertTrue(session().isTextPresent(parentDocumentClickabletext));
-	//verify if text of parent is not present
-	assertFalse(session().isTextPresent(parentText));
-	
-	//verify if text of parent is present in child site
-	session().selectFrame("relative=parent");
-	goToSite(childSiteName);
-	openTeachingMaterialSection();
-	assertTrue(session().isTextPresent(parentDocumentClickabletext));
-	//verify if text of parent is not present in child site
-	assertFalse(session().isTextPresent(parentText));
-	
-	// dissociate current site form parent
-	session().selectFrame("relative=parent");
-	goToOsylManagerTool();
-	session().type("//input[@class='gwt-TextBox']", getCurrentTestSiteName());
-	session().mouseOver("//div[@class='Osyl-Button Osyl-Button-up']");
-	session().mouseDown("//div[@class='Osyl-Button Osyl-Button-up-hovering']");
-	session().mouseUp("//div[@class='Osyl-Button Osyl-Button-down-hovering']");
-	pause();
-	session().click("//tr[@class='OsylManager-scrollTable-row']/td/input");
-	pause();
-	session().mouseOver("//div[@class='OsylManager-action OsylManager-action-up' and ./div[contains(.,'tacher')]]");
-	session().mouseDown("//div[@class='OsylManager-action OsylManager-action-up-hovering' and ./div[contains(.,'tacher')]]");
-	session().mouseUp("//div[@class='OsylManager-action OsylManager-action-down-hovering' and ./div[contains(.,'tacher')]]");
-	pause();
-	
-	//verify if text is not present in current site
-	goToCurrentSite();
-	openTeachingMaterialSection();
-	assertFalse(session().isTextPresent(parentDocumentClickabletext));
-	
-	//verify if text is not present in child site
-	session().selectFrame("relative=parent");
-	goToSite(childSiteName);
-	openTeachingMaterialSection();
-	assertFalse(session().isTextPresent(parentDocumentClickabletext));
-	// verify if text from current site is present
-	assertTrue(session().isTextPresent(currentSiteText));
-	
-	// dissociate child from current site name
-	session().selectFrame("relative=parent");
-	goToOsylManagerTool();
-	session().type("//input[@class='gwt-TextBox']", childSiteName);
-	session().mouseOver("//div[@class='Osyl-Button Osyl-Button-up']");
-	session().mouseDown("//div[@class='Osyl-Button Osyl-Button-up-hovering']");
-	session().mouseUp("//div[@class='Osyl-Button Osyl-Button-down-hovering']");
-	pause();
-	session().click("//tr[@class='OsylManager-scrollTable-row']/td/input");
-	pause();
-	session().mouseOver("//div[@class='OsylManager-action OsylManager-action-up' and ./div[contains(.,'tacher')]]");
-	session().mouseDown("//div[@class='OsylManager-action OsylManager-action-up-hovering' and ./div[contains(.,'tacher')]]");
-	session().mouseUp("//div[@class='OsylManager-action OsylManager-action-down-hovering' and ./div[contains(.,'tacher')]]");
-	pause();
-	
-	//verify if text from current site is not present
-	goToSite(childSiteName);
-	openTeachingMaterialSection();
-	assertFalse(session().isTextPresent(currentSiteText));
-	
-	logOut();
-	log("testAddLecture: test complete");
-	}catch(Exception e){
-	    logAndFail("Associate to parent FAILED:"+e);
+	    String parentText =
+		    "this is a text resource typed by "
+			    + "selenium, hope it works and you see it. Added on "
+			    + timeStamp() + " in Firefox";
+
+	    addText(parentText, LEVEL_ATTENDEE);
+
+	    // publish
+	    session().click("gwt-uid-5");
+	    session().click("//button[contains(text(),'Publier')]");
+	    pause();
+
+	    // attach to parent
+	    session().selectFrame("relative=parent");
+	    goToOsylManagerTool();
+	    session().type("//input[@class='gwt-TextBox']",
+		    getCurrentTestSiteName());
+	    session().mouseOver("//div[@class='Osyl-Button Osyl-Button-up']");
+	    session().mouseDown(
+		    "//div[@class='Osyl-Button Osyl-Button-up-hovering']");
+	    session().mouseUp(
+		    "//div[@class='Osyl-Button Osyl-Button-down-hovering']");
+	    pause();
+	    session().click(
+		    "//tr[@class='OsylManager-scrollTable-row']/td/input");
+	    pause();
+	    assertTrue(session().isElementPresent("//div[@class='OsylManager-action OsylManager-action-up' and ./div[contains(.,'Lier')]]"));
+	    session()
+		    .mouseOver(
+			    "//div[@class='OsylManager-action OsylManager-action-up' and ./div[contains(.,'Lier')]]");
+	    session()
+		    .mouseDown(
+			    "//div[@class='OsylManager-action OsylManager-action-up-hovering' and ./div[contains(.,'Lier')]]");
+	    session()
+		    .mouseUp(
+			    "//div[@class='OsylManager-action OsylManager-action-down-hovering' and ./div[contains(.,'Lier')]]");
+	    pause();
+	    session().select("//select[@class='OsylManager-form-element']",
+		    "label=" + parentSiteName);
+	    pause();
+	    session()
+		    .mouseOver(
+			    "//div[@class='Osyl-Button Osyl-Button-up' and ./div[contains(.,'Lier')]]");
+	    session()
+		    .mouseDown(
+			    "//div[@class='Osyl-Button Osyl-Button-up-hovering' and ./div[contains(.,'Lier')]]");
+	    session()
+		    .mouseUp(
+			    "//div[@class='Osyl-Button Osyl-Button-down-hovering' and ./div[contains(.,'Lier')]]");
+	    pause();
+	    assertTrue(session().isElementPresent("//div[contains(.,'bien')]"));
+	    session()
+		    .mouseOver(
+			    "//div[@class='Osyl-Button Osyl-Button-up' and ./div[contains(.,'Fermer')]]");
+	    session()
+		    .mouseDown(
+			    "//div[@class='Osyl-Button Osyl-Button-up-hovering' and ./div[contains(.,'Fermer')]]");
+	    session()
+		    .mouseUp(
+			    "//div[@class='Osyl-Button Osyl-Button-down-hovering' and ./div[contains(.,'Fermer')]]");
+
+	    // verify if text is present
+	    goToCurrentSite();
+	    openTeachingMaterialSection();
+	    assertTrue(session().isTextPresent(parentText));
+
+	    String currentSiteText =
+		    "this is a text resource typed by "
+			    + "selenium, hope it works and you see it. Added on "
+			    + timeStamp() + " in Firefox";
+	    addText(currentSiteText, LEVEL_ATTENDEE);
+
+	    // publish
+	    session().click("gwt-uid-5");
+	    session().click("//button[contains(text(),'Publier')]");
+	    pause();
+
+	    // attach child to current site
+	    session().selectFrame("relative=parent");
+	    goToOsylManagerTool();
+	    session().type("//input[@class='gwt-TextBox']", childSiteName);
+	    session().mouseOver("//div[@class='Osyl-Button Osyl-Button-up']");
+	    session().mouseDown(
+		    "//div[@class='Osyl-Button Osyl-Button-up-hovering']");
+	    session().mouseUp(
+		    "//div[@class='Osyl-Button Osyl-Button-down-hovering']");
+	    pause();
+	    session().click(
+		    "//tr[@class='OsylManager-scrollTable-row']/td/input");
+	    pause();
+	    assertTrue(session().isElementPresent("//div[@class='OsylManager-action OsylManager-action-up' and ./div[contains(.,'Lier')]]"));
+	    session()
+		    .mouseOver(
+			    "//div[@class='OsylManager-action OsylManager-action-up' and ./div[contains(.,'Lier')]]");
+	    session()
+		    .mouseDown(
+			    "//div[@class='OsylManager-action OsylManager-action-up-hovering' and ./div[contains(.,'Lier')]]");
+	    session()
+		    .mouseUp(
+			    "//div[@class='OsylManager-action OsylManager-action-down-hovering' and ./div[contains(.,'Lier')]]");
+	    pause();
+	    session().select("//select[@class='OsylManager-form-element']",
+		    "label=" + getCurrentTestSiteName());
+	    pause();
+	    session()
+		    .mouseOver(
+			    "//div[@class='Osyl-Button Osyl-Button-up' and ./div[contains(.,'Lier')]]");
+	    session()
+		    .mouseDown(
+			    "//div[@class='Osyl-Button Osyl-Button-up-hovering' and ./div[contains(.,'Lier')]]");
+	    session()
+		    .mouseUp(
+			    "//div[@class='Osyl-Button Osyl-Button-down-hovering' and ./div[contains(.,'Lier')]]");
+	    pause();
+	    assertTrue(session().isElementPresent("//div[contains(.,'bien')]"));
+	    session()
+		    .mouseOver(
+			    "//div[@class='Osyl-Button Osyl-Button-up' and ./div[contains(.,'Fermer')]]");
+	    session()
+		    .mouseDown(
+			    "//div[@class='Osyl-Button Osyl-Button-up-hovering' and ./div[contains(.,'Fermer')]]");
+	    session()
+		    .mouseUp(
+			    "//div[@class='Osyl-Button Osyl-Button-down-hovering' and ./div[contains(.,'Fermer')]]");
+
+	    // verify if text is present
+	    goToSite(childSiteName);
+	    openTeachingMaterialSection();
+	    assertTrue(session().isTextPresent(parentText));
+	    assertTrue(session().isTextPresent(currentSiteText));
+
+	    // go to parent site
+	    session().selectFrame("relative=parent");
+	    goToSite(parentSiteName);
+	    openTeachingMaterialSection();
+
+	    // delete text
+	    // We delete new added docuement
+	    session().click("//tr/td/div/table[2]/tbody/tr/td[2]/button");
+	    pause();
+
+	    session()
+		    .click(
+			    "//tr/td[2]/div/table/tbody/tr[2]/td/table/tbody/tr/td/button");
+	    pause();
+
+	    // ---------------------------------------------------------------------------//
+	    // Add Document //
+	    // ---------------------------------------------------------------------------//
+	    String parentDocumentClickabletext = timeStamp();
+	    addDocument("osyl-src[1].zip", parentDocumentClickabletext);
+
+	    // publish
+	    session().click("gwt-uid-5");
+	    session().click("//button[contains(text(),'Publier')]");
+	    pause();
+
+	    // verify if text of parentis present in current site
+	    session().selectFrame("relative=parent");
+	    goToCurrentSite();
+	    openTeachingMaterialSection();
+	    assertTrue(session().isTextPresent(parentDocumentClickabletext));
+	    // verify if text of parent is not present
+	    assertFalse(session().isTextPresent(parentText));
+
+	    // verify if text of parent is present in child site
+	    session().selectFrame("relative=parent");
+	    goToSite(childSiteName);
+	    openTeachingMaterialSection();
+	    assertTrue(session().isTextPresent(parentDocumentClickabletext));
+	    // verify if text of parent is not present in child site
+	    assertFalse(session().isTextPresent(parentText));
+
+	    // dissociate current site form parent
+	    session().selectFrame("relative=parent");
+	    goToOsylManagerTool();
+	    session().type("//input[@class='gwt-TextBox']",
+		    getCurrentTestSiteName());
+	    session().mouseOver("//div[@class='Osyl-Button Osyl-Button-up']");
+	    session().mouseDown(
+		    "//div[@class='Osyl-Button Osyl-Button-up-hovering']");
+	    session().mouseUp(
+		    "//div[@class='Osyl-Button Osyl-Button-down-hovering']");
+	    pause();
+	    session().click(
+		    "//tr[@class='OsylManager-scrollTable-row']/td/input");
+	    pause();
+	    assertTrue(session().isElementPresent("//div[@class='OsylManager-action OsylManager-action-up' and ./div[contains(.,'tacher')]]"));
+	    session()
+		    .mouseOver(
+			    "//div[@class='OsylManager-action OsylManager-action-up' and ./div[contains(.,'tacher')]]");
+	    session()
+		    .mouseDown(
+			    "//div[@class='OsylManager-action OsylManager-action-up-hovering' and ./div[contains(.,'tacher')]]");
+	    session()
+		    .mouseUp(
+			    "//div[@class='OsylManager-action OsylManager-action-down-hovering' and ./div[contains(.,'tacher')]]");
+	    pause();
+
+	    // verify if text is not present in current site
+	    goToCurrentSite();
+	    openTeachingMaterialSection();
+	    assertFalse(session().isTextPresent(parentDocumentClickabletext));
+
+	    // verify if text is not present in child site
+	    session().selectFrame("relative=parent");
+	    goToSite(childSiteName);
+	    openTeachingMaterialSection();
+	    assertFalse(session().isTextPresent(parentDocumentClickabletext));
+	    // verify if text from current site is present
+	    assertTrue(session().isTextPresent(currentSiteText));
+
+	    // dissociate child from current site name
+	    session().selectFrame("relative=parent");
+	    goToOsylManagerTool();
+	    session().type("//input[@class='gwt-TextBox']", childSiteName);
+	    session().mouseOver("//div[@class='Osyl-Button Osyl-Button-up']");
+	    session().mouseDown(
+		    "//div[@class='Osyl-Button Osyl-Button-up-hovering']");
+	    session().mouseUp(
+		    "//div[@class='Osyl-Button Osyl-Button-down-hovering']");
+	    pause();
+	    session().click(
+		    "//tr[@class='OsylManager-scrollTable-row']/td/input");
+	    pause();
+	    assertTrue(session().isElementPresent("//div[@class='OsylManager-action OsylManager-action-up' and ./div[contains(.,'tacher')]]"));
+	    session()
+		    .mouseOver(
+			    "//div[@class='OsylManager-action OsylManager-action-up' and ./div[contains(.,'tacher')]]");
+	    session()
+		    .mouseDown(
+			    "//div[@class='OsylManager-action OsylManager-action-up-hovering' and ./div[contains(.,'tacher')]]");
+	    session()
+		    .mouseUp(
+			    "//div[@class='OsylManager-action OsylManager-action-down-hovering' and ./div[contains(.,'tacher')]]");
+	    pause();
+
+	    // verify if text from current site is not present
+	    goToSite(childSiteName);
+	    openTeachingMaterialSection();
+	    assertFalse(session().isTextPresent(currentSiteText));
+
+	    logOut();
+	    log("testAddLecture: test complete");
+	} catch (Exception e) {
+	    logAndFail("Associate to parent FAILED:" + e);
 	}
     } // testAddLecture
-    
-    
-    
+
 }
