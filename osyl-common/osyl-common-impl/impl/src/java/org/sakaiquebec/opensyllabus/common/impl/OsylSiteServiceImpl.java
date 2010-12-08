b952611e3b4cc4b94347756820c407bf85101776
Laurent Danet <laurent.danet@hec.ca>
2010-12-08 14:17:31 +0000

    SAKAI-1674 : login log

--- osyl-common/osyl-common-impl/impl/src/java/org/sakaiquebec/opensyllabus/common/impl/OsylSiteServiceImpl.java
+++ osyl-common/osyl-common-impl/impl/src/java/org/sakaiquebec/opensyllabus/common/impl/OsylSiteServiceImpl.java
@@ -22,6 +22,10 @@ package org.sakaiquebec.opensyllabus.common.impl;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
@@ -302,7 +306,11 @@ public class OsylSiteServiceImpl implements OsylSiteService, EntityTransferrer {
 
 	    public void update(Observable o, Object arg) {
 		Event e = (Event) arg;
-		if (e.getEvent().equals(UsageSessionService.EVENT_LOGOUT)) {
+		if(e.getEvent().equals(UsageSessionService.EVENT_LOGIN)){
+		    DateFormat df = new SimpleDateFormat("dd/MM/yyyy 'at' HH:mm");
+		    log.info("user [" + sessionManager.getCurrentSession().getUserEid()
+		+ "] login "+df.format(new Date()));
+		}else if (e.getEvent().equals(UsageSessionService.EVENT_LOGOUT)) {
 		    // we unlocks all CO locks by current user (he logs out)
 		    resourceDao.clearLocksForSession(sessionManager
 			    .getCurrentSession().getId());
