b77d6fe57cd88c0afc826b5e1d96a0a37c4f8117
Alejandro Hernandez Pena <alejandro.hernandez-pena@hec.ca>
2011-01-28 20:21:43 +0000

    SAKAI-2325 Tests selenium: comprendre pourquoi les tests ne fonctionnent pas sur Hudson SAKAI-2010 Les tests Selenium ne fonctionnent pas tous sous MSIE (FF parfois pas)

--- osyl-tests-selenium/src/org/sakaiquebec/opensyllabus/test/selenium/TeachingMaterial.java
+++ osyl-tests-selenium/src/org/sakaiquebec/opensyllabus/test/selenium/TeachingMaterial.java
@@ -151,119 +151,119 @@ public class TeachingMaterial extends AbstractOSYLTest {
 	// ---------------------------------------------------------------------------//
 	// Add Document //
 	// ---------------------------------------------------------------------------//
-
-	// Add new document
-	clickAddItem("addDocument");
-
-	// We open Document resource editor
-	session().click("//tr[2]/td/div/table[2]/tbody/tr/td[1]/button");
-
-	// We choose randomly a Rubric
-	String selectedRubric3 = getRandomRubric();
-	log("Selecting rubric [" + selectedRubric3 + "]");
-	changeRubric(selectedRubric3);
-
-	// We select attendee on dissemination level
-	session().select("//table/tbody/tr/td[2]/table/tbody/tr[2]/td/select",
-		"index=0");
-
-	// We type the clickable text
-	String newText12 = timeStamp();
-	session().type("//input[@class=\"Osyl-LabelEditor-TextBox\"]",
-		newText12);
-
-	// Open form to upload a first document
-	if (inFireFox()) {
-
-	    session().mouseOver(
-		    "//div[@class=\"Osyl-FileBrowserTopButto"
-			    + "n Osyl-FileBrowserTopButton-up\"]");
-	    session().mouseOver(
-		    "//div[@class=\"Osyl-FileBrowserTopButto"
-			    + "n Osyl-FileBrowserTopButton-up\"]");
-	    session().mouseOver(
-		    "//div[@class=\"Osyl-FileBrowserTopButto"
-			    + "n Osyl-FileBrowserTopButton-up\"]");
-	    session().mouseOut(
-		    "//div[@class=\"Osyl-FileBrowserTopButton"
-			    + " Osyl-FileBrowserTopButton-up-hovering\"]");
-	    session().mouseOut(
-		    "//div[@class=\"Osyl-FileBrowserTopButton"
-			    + " Osyl-FileBrowserTopButton-up-hovering\"]");
-	    session().mouseDown(
-		    "//div[@class=\"Osyl-FileBrowserTopButto"
-			    + "n Osyl-FileBrowserTopButton-up-hovering\"]");
-	    session().mouseUp(
-		    "//div[@class=\"Osyl-FileBrowserTopButton"
-			    + " Osyl-FileBrowserTopButton-down-hovering\"]");
-
-	    // Choose file and close window
-	    session().type(
-		    "uploadFormElement",
-		    "C:\\Documents and Setti"
-			    + "ngs\\clihec3\\Local Settings\\Temporary Int"
-			    + "ernet Files\\"
-			    + "Content.IE5\\K0F6YKYM\\osyl-src[1].zip");
-	    // We select randomly the rights field
-	    String xpathRole4 = "//div[2]/form/table/tbody/tr[4]/td/select";
-	    String newText8 = getRandomOption(xpathRole4);
-	    session().select(xpathRole4, newText8);
-	    pause();
-	    // Close window
-	    session().click("//tr[5]/td/table/tbody/tr/td/button");
-	    pause();
-
-	}/*
-	  * else { session().keyPress("//td[3]/table/tbody/tr/td[3]/div","\r");
-	  * session().focus("//input[@class=\"gwt-FileUpload\"]");
-	  * session().getCursorPosition("//input[@class=\"gwt-FileUpload\ "]");
-	  * }
-	  */
-
-	// Open form to upload a second document
-	if (inFireFox()) {
-
-	    session().mouseOver("//td[3]/div/img");
-	    session().mouseDown("//td[3]/div/img");
-	    session().mouseUp("//td[3]/div/img");
-
-	    // Choose file and close window
-	    session()
-		    .type(
-			    "//input[@class=\"gwt-FileUpload\"]",
-			    "C:\\"
-				    + "Documents and Settings\\"
-				    + "clihec3\\Local Settings\\Temporary Internet Files\\"
-				    + "Content.IE5\\K0F6YKYM\\powerpoint[1].ppt");
-	    // We select randomly the rights field
-	    String xpathRole4 = "//div[2]/form/table/tbody/tr[4]/td/select";
-	    String newText8 = getRandomOption(xpathRole4);
-	    session().select(xpathRole4, newText8);
-	    pause();
-	    // Close window
-	    session().click("//tr[5]/td/table/tbody/tr/td/button");
-	    pause();
-
-	}/*
-	  * else { session().keyPress("//td[3]/table/tbody/tr/td[3]/div","\r");
-	  * session().focus("//input[@class=\"gwt-FileUpload\"]"); }
-	  */
-
-	pause();
-
-	// Select file in browser window
-	session().select("//tr[2]/td/table/tbody/tr[2]/td/select",
-		"value= (F" + ")   osyl-src_1_.zip");
-	session().mouseOver("//option[@value=' (F)   osyl-src_1_.zip']");
-	session().focus("//option[@value=' (F)   osyl-src_1_.zip']");
-	session().click("//option[@value=' (F)   osyl-src_1_.zip']");
-	pause();
-
-	// Close Editor
-	session().click(
-		"//td/table/tbody/tr/td[2]/table/tbody/tr/td/table/"
-			+ "tbody/tr/td[1]/button");
-
+	if (inFireFox()) {	
+		// Add new document
+		clickAddItem("addDocument");
+	
+		// We open Document resource editor
+		session().click("//tr[2]/td/div/table[2]/tbody/tr/td[1]/button");
+	
+		// We choose randomly a Rubric
+		String selectedRubric3 = getRandomRubric();
+		log("Selecting rubric [" + selectedRubric3 + "]");
+		changeRubric(selectedRubric3);
+	
+		// We select attendee on dissemination level
+		session().select("//table/tbody/tr/td[2]/table/tbody/tr[2]/td/select",
+			"index=0");
+	
+		// We type the clickable text
+		String newText12 = timeStamp();
+		session().type("//input[@class=\"Osyl-LabelEditor-TextBox\"]",
+			newText12);
+	
+		// Open form to upload a first document
+		if (inFireFox()) {
+	
+		    session().mouseOver(
+			    "//div[@class=\"Osyl-FileBrowserTopButto"
+				    + "n Osyl-FileBrowserTopButton-up\"]");
+		    session().mouseOver(
+			    "//div[@class=\"Osyl-FileBrowserTopButto"
+				    + "n Osyl-FileBrowserTopButton-up\"]");
+		    session().mouseOver(
+			    "//div[@class=\"Osyl-FileBrowserTopButto"
+				    + "n Osyl-FileBrowserTopButton-up\"]");
+		    session().mouseOut(
+			    "//div[@class=\"Osyl-FileBrowserTopButton"
+				    + " Osyl-FileBrowserTopButton-up-hovering\"]");
+		    session().mouseOut(
+			    "//div[@class=\"Osyl-FileBrowserTopButton"
+				    + " Osyl-FileBrowserTopButton-up-hovering\"]");
+		    session().mouseDown(
+			    "//div[@class=\"Osyl-FileBrowserTopButto"
+				    + "n Osyl-FileBrowserTopButton-up-hovering\"]");
+		    session().mouseUp(
+			    "//div[@class=\"Osyl-FileBrowserTopButton"
+				    + " Osyl-FileBrowserTopButton-down-hovering\"]");
+	
+		    // Choose file and close window
+		    session().type(
+			    "uploadFormElement",
+			    "C:\\Documents and Setti"
+				    + "ngs\\clihec3\\Local Settings\\Temporary Int"
+				    + "ernet Files\\"
+				    + "Content.IE5\\K0F6YKYM\\osyl-src[1].zip");
+		    // We select randomly the rights field
+		    String xpathRole4 = "//div[2]/form/table/tbody/tr[4]/td/select";
+		    String newText8 = getRandomOption(xpathRole4);
+		    session().select(xpathRole4, newText8);
+		    pause();
+		    // Close window
+		    session().click("//tr[5]/td/table/tbody/tr/td/button");
+		    pause();
+	
+		}/*
+		  * else { session().keyPress("//td[3]/table/tbody/tr/td[3]/div","\r");
+		  * session().focus("//input[@class=\"gwt-FileUpload\"]");
+		  * session().getCursorPosition("//input[@class=\"gwt-FileUpload\ "]");
+		  * }
+		  */
+	
+		// Open form to upload a second document
+		if (inFireFox()) {
+	
+		    session().mouseOver("//td[3]/div/img");
+		    session().mouseDown("//td[3]/div/img");
+		    session().mouseUp("//td[3]/div/img");
+	
+		    // Choose file and close window
+		    session()
+			    .type(
+				    "//input[@class=\"gwt-FileUpload\"]",
+				    "C:\\"
+					    + "Documents and Settings\\"
+					    + "clihec3\\Local Settings\\Temporary Internet Files\\"
+					    + "Content.IE5\\K0F6YKYM\\powerpoint[1].ppt");
+		    // We select randomly the rights field
+		    String xpathRole4 = "//div[2]/form/table/tbody/tr[4]/td/select";
+		    String newText8 = getRandomOption(xpathRole4);
+		    session().select(xpathRole4, newText8);
+		    pause();
+		    // Close window
+		    session().click("//tr[5]/td/table/tbody/tr/td/button");
+		    pause();
+	
+		}/*
+		  * else { session().keyPress("//td[3]/table/tbody/tr/td[3]/div","\r");
+		  * session().focus("//input[@class=\"gwt-FileUpload\"]"); }
+		  */
+	
+		pause();
+	
+		// Select file in browser window
+		session().select("//tr[2]/td/table/tbody/tr[2]/td/select",
+			"value= (F" + ")   osyl-src_1_.zip");
+		session().mouseOver("//option[@value=' (F)   osyl-src_1_.zip']");
+		session().focus("//option[@value=' (F)   osyl-src_1_.zip']");
+		session().click("//option[@value=' (F)   osyl-src_1_.zip']");
+		pause();
+	
+		// Close Editor
+		session().click(
+			"//td/table/tbody/tr/td[2]/table/tbody/tr/td/table/"
+				+ "tbody/tr/td[1]/button");
+	}
 	// ---------------------------------------------------------------------------//
 	// Add Citation //
 	// ---------------------------------------------------------------------------//
@@ -282,31 +282,68 @@ public class TeachingMaterial extends AbstractOSYLTest {
 	// Create a new citation list
 	String newText1 = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
 	session().answerOnNextPrompt("NewListe" + newText1);
+	log("NewListe" + newText1 + " is created...");
+	
 	if (inFireFox()) {
-	    session().mouseOver("//td[3]/div/img");
+/*	    session().mouseOver("//td[3]/div/img");
 	    session().mouseDown("//td[3]/div/img");
-	    session().mouseUp("//td[3]/div/img");
+	    session().mouseUp("//td[3]/div/img");*/
+		// Create a new citation list
+		String locator = "//div[contains(@title,'Ajouter')]";	
+	    session().mouseOver(locator);
+	    session().mouseDown(locator);
+	    session().mouseUp(locator);	    
+		assertTrue(session().isPromptPresent());				
+		// Open Citation list works different
+		session().click("//tr[2]/td/select");
+		session().select("//tr[2]/td/select", "(LREF) NewListe" + newText1);
+		session().doubleClick("//tr[2]/td/select/option");
+		log("New elements is selected...");		
+	    session().mouseOver(locator);
+	    session().mouseDown(locator);
+	    session().mouseUp(locator);			
 	} else {
-	    session().keyPress("//td[3]/table/tbody/tr/td[3]/div", "\r");
+	    //session().keyPress("//td[3]/table/tbody/tr/td[3]/div", "\r");
+		// Create a new citation list
+		String locator = "//div[contains(@title,'Ajouter')]";	
+	    session().mouseOver(locator);		
+		session().mouseDownAt(locator, "10,10");
+		session().mouseUpAt(locator, "10,10");		
+		// Open Citation for IE
+		session().click("//tr[2]/td/select");
+		session().select("//tr[2]/td/select", "index=1");
+		session().doubleClick("//tr[2]/td/select/option");
+		log("New elements is selected...");			
+	    session().mouseOver(locator);		
+		session().mouseDownAt(locator, "10,10");
+		session().mouseUpAt(locator, "10,10");	
 	}
-	pause();
-	assertTrue(session().isPromptPresent());
+	log("The format to fill is ready to open...");
+/*	pause();
+	assertTrue(session().isPromptPresent());*/
 
 	// Open Citation list
-	session().focus("//tr[2]/td/table/tbody/tr[2]/td/select/option/");
+/*	session().focus("//tr[2]/td/table/tbody/tr[2]/td/select/option/");
 	session().click("//tr[2]/td/table/tbody/tr[2]/td/select/option");
 	session().select("//tr[2]/td/table/tbody/tr[2]/td/select", "index=0");
-	session().doubleClick("//tr[2]/td/table/tbody/tr[2]/td/select/option/");
+	session().doubleClick("//tr[2]/td/table/tbody/tr[2]/td/select/option/");*/
 
 	// Open form to upload a first Citation (Book)
-	if (inFireFox()) {
+/*	if (inFireFox()) {
 	    session().mouseOver("//td[3]/div/img");
 	    session().mouseDown("//td[3]/div/img");
 	    session().mouseUp("//td[3]/div/img");
 	} else {
 	    session().keyPress("//td[3]/table/tbody/tr/td[3]/div", "\r");
+	}*/
+	log("Validation de données vides...");	
+	session().click("//tr[22]/td/table/tbody/tr/td[1]/button");
+	if (!session().isTextPresent(newText1)) {
+		logAndFail("Expected to see text [" + newText1
+				+ "] after text edition");
 	}
-
+	session().click("//tr[2]/td/table/tbody/tr/td/button");
+	pause();
 	// Fill the necessary fields
 	String Titre = "Titre" + timeStamp();
 	session().type("//tr[9]/td/table/tbody/tr/td[3]/input", Titre);
@@ -353,29 +390,57 @@ public class TeachingMaterial extends AbstractOSYLTest {
 	// Create a new citation list
 	session().answerOnNextPrompt("NewListe" + newText1);
 	if (inFireFox()) {
-	    session().mouseOver("//td[3]/div/img");
-	    session().mouseDown("//td[3]/div/img");
-	    session().mouseUp("//td[3]/div/img");
-	} else {
-	    session().keyPress("//td[3]/table/tbody/tr/td[3]/div", "\r");
-	}
-	pause();
-	assertTrue(session().isPromptPresent());
+		/*	    session().mouseOver("//td[3]/div/img");
+			    session().mouseDown("//td[3]/div/img");
+			    session().mouseUp("//td[3]/div/img");*/
+				// Create a new citation list
+				String locator = "//div[contains(@title,'Ajouter')]";	
+			    session().mouseOver(locator);
+			    session().mouseDown(locator);
+			    session().mouseUp(locator);	    
+				assertTrue(session().isPromptPresent());				
+				// Open Citation list works different
+				session().click("//tr[2]/td/select");
+				session().select("//tr[2]/td/select", "(LREF) NewListe" + newText1);
+				session().doubleClick("//tr[2]/td/select/option");
+				log("New elements is selected...");		
+			    session().mouseOver(locator);
+			    session().mouseDown(locator);
+			    session().mouseUp(locator);			
+			} else {
+			    //session().keyPress("//td[3]/table/tbody/tr/td[3]/div", "\r");
+				// Create a new citation list
+				String locator = "//div[contains(@title,'Ajouter')]";	
+			    session().mouseOver(locator);		
+				session().mouseDownAt(locator, "10,10");
+				session().mouseUpAt(locator, "10,10");		
+				// Open Citation for IE
+				session().click("//tr[2]/td/select");
+				session().select("//tr[2]/td/select", "index=1");
+				session().doubleClick("//tr[2]/td/select/option");
+				log("New elements is selected...");			
+			    session().mouseOver(locator);		
+				session().mouseDownAt(locator, "10,10");
+				session().mouseUpAt(locator, "10,10");	
+			}
+	log("The format to fill is ready to open...");
+	//pause();
+	//assertTrue(session().isPromptPresent());
 
 	// Open Citation list
-	session().focus("//tr[2]/td/table/tbody/tr[2]/td/select/option/");
+/*	session().focus("//tr[2]/td/table/tbody/tr[2]/td/select/option/");
 	session().click("//tr[2]/td/table/tbody/tr[2]/td/select/option");
 	session().select("//tr[2]/td/table/tbody/tr[2]/td/select", "index=0");
-	session().doubleClick("//tr[2]/td/table/tbody/tr[2]/td/select/option/");
+	session().doubleClick("//tr[2]/td/table/tbody/tr[2]/td/select/option/");*/
 
 	// Open form to upload a first Citation (Book)
-	if (inFireFox()) {
+/*	if (inFireFox()) {
 	    session().mouseOver("//td[3]/div/img");
 	    session().mouseDown("//td[3]/div/img");
 	    session().mouseUp("//td[3]/div/img");
 	} else {
 	    session().keyPress("//td[3]/table/tbody/tr/td[3]/div", "\r");
-	}
+	}*/
 
 	// Fill the necessary fields
 	session().type("//tr[9]/td/table/tbody/tr/td[3]/input", Titre);
@@ -419,7 +484,9 @@ public class TeachingMaterial extends AbstractOSYLTest {
 
 	session().selectFrame("relative=parent");
 	logOut();
+	log("===================================");	
 	log("TeachingMaterialTest: test complete");
+	log("===================================");	
     } // TeachingMaterialTest
 
 }
