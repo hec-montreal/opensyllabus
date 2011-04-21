50961611537bff3abe664d70c51b4adcfb17ba6c
Alejandro Hernandez Pena <alejandro.hernandez-pena@hec.ca>
2011-04-21 17:52:14 +0000

    SAKAI-2625 Implantation: figer les sites après la session

---
+++ osyl-admin/osyl-admin-server-api/api/src/java/org/sakaiquebec/opensyllabus/admin/cmjob/api/FreezeSitesAfterSessionJob.java
@@ -0,0 +1,70 @@
+package org.sakaiquebec.opensyllabus.admin.cmjob.api;
+
+import java.util.List;
+
+import org.quartz.Job;
+
+import java.util.Arrays;
+
+/******************************************************************************
+ * $Id: $
+ ******************************************************************************
+ *
+ * Copyright (c) 2010 The Sakai Foundation, The Sakai Quebec Team.
+ *
+ * Licensed under the Educational Community License, Version 1.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *****************************************************************************/
+
+/**
+ * This job takes away several permissions on all sites corresponding to session 
+ * period identified in a XML configuration file.
+ * Permissions to be taken away: osyl.edit, osyl.publish, asn, etc. (to validate)
+ * an objective being to allow permissions to read and copy 
+ * 
+ * @author <a href="mailto:alejandro.hernandez-pena@hec.ca">Alejandro Hernandez</a>
+ * @version $Id: $
+ */
+public interface FreezeSitesAfterSessionJob extends Job {
+
+	// These are the functions that will allowed to these sites. If we encounter
+	// any other function the job will try to remove it.
+	
+	public final static String[] FUNCTIONS_TO_ALLOW = {"annc.read", "asn.read", "assessment.questionpool.copy.own",
+			"content.read", "osyl.read", "site.visit", "sitestats.view"};  
+	
+	public final static List<String> listFunctions = Arrays.asList(FUNCTIONS_TO_ALLOW);  
+		
+	// File containing the xml configuration of sites to freeze
+	public final static String FROZENSITESCONFIG = "frozenSitesConfig.xml";
+
+    public final static String ADMINSITENAME = "opensyllabusAdmin";
+
+	//Folder that contains all the configuration files
+	public final static String ADMIN_CONTENT_FOLDER = "/content/group/opensyllabusAdmin/";
+    public final static String CONFIGFORLDER = ADMIN_CONTENT_FOLDER + "config/";
+
+    // Tags used in the xml files
+    // For the official sites xml configuration file
+    public final static String SESSION = "session";
+    public final static String PERIOD = "period";
+    
+	// Sessions
+	public final static String SUMMER = "E";
+	public final static String WINTER = "H";
+	public final static String FALL = "A";
+	
+    public final static String REALM_PREFIX = "/site/";
+
+}
