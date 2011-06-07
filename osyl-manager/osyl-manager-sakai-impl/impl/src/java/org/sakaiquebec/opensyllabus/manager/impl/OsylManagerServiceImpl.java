067e96ac1b1724ceddce1998139d736123ac8124
Alejandro Hernandez Pena <alejandro.hernandez-pena@hec.ca>
2011-06-07 13:15:17 +0000

    SAKAI-2623 PCA: intégration dans le GDPC

--- osyl-manager/osyl-manager-sakai-impl/impl/src/java/org/sakaiquebec/opensyllabus/manager/impl/OsylManagerServiceImpl.java
+++ osyl-manager/osyl-manager-sakai-impl/impl/src/java/org/sakaiquebec/opensyllabus/manager/impl/OsylManagerServiceImpl.java
@@ -1934,7 +1934,7 @@ public class OsylManagerServiceImpl implements OsylManagerService {
 	}
 
 	// We remove all resources in the publish directory collection
-	osylContentService.initSiteAttachments(newSite.getTitle());
+	osylContentService.initSiteAttachments(newSite);
 
 	// we hide work directory
 	ContentCollectionEdit cce =
