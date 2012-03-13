55e737481cbcb91b9f09746f9de9e102afc7d31e
Thanh Nguyen <thanh.2.nguyen@hec.ca>
2012-03-13 22:16:36 +0000

    SAKAI-2968: Fixer une partie des tests (9)

---
+++ osyl-tests-selenium/run-server.bat
@@ -0,0 +1,1 @@
+java -jar lib\selenium-server-standalone-2.19.0.jar
---
+++ osyl-tests-selenium/run2-dev.bat
@@ -0,0 +1,2 @@
+java -cp classes;lib\testng-5.9-jdk15.jar;lib\selenium-server-standalone-2.19.0.jar;lib\selenium-grid-tools-1.0.8.jar;lib\commons-logging-1.0.4.jar org.testng.TestNG OSYLTestSuite-dev.xml
+
