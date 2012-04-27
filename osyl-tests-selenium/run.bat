a74efbd184571325650afe716fc9e47e35f4f136
Thanh Nguyen <thanh.2.nguyen@hec.ca>
2012-04-27 14:37:29 +0000

    SAKAI-3235: Migrer les tests automatisés vers la nouvelle version 2.8

--- osyl-tests-selenium/run.bat
+++ osyl-tests-selenium/run.bat
@@ -1,1 +1,8 @@
-java -classpath classes;lib\testng-5.9-jdk15.jar;lib\selenium-java-client-driver.jar;lib\selenium-grid-tools-1.0.3.jar;lib\junit-4.6.jar;lib\commons-logging-1.0.4.jar org.testng.TestNG OSYLTestSuite.xml
+java -cp classes;lib\testng-5.9-jdk15.jar;lib\selenium-server-standalone-2.19.0.jar;lib\selenium-grid-tools-1.0.8.jar;lib\commons-logging-1.0.4.jar org.testng.TestNG OSYLTestSuite-dev.xml
+
+
+REM rename file according to date
+set fileOut=TestReport_281_%date%_%time:~0,2%%time:~3,2%.html
+copy test-output\emailable-report.html test-output\%fileOut% 
+
+
