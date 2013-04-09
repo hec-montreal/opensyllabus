797702df1dd530c45c0d05f5bbff6efd1198413c
Cyril Mace <cyril.mace@hec.ca>
2013-04-09 19:46:31 +0000

    ZCII-531: Initialiser la classe de convertissage dans HEC-COMMON-UTILS

---
+++ osyl-common/osyl-common-shared/src/java/org/sakaiquebec/opensyllabus/shared/util/CitationsUtils.java
@@ -0,0 +1,13 @@
+package org.sakaiquebec.opensyllabus.shared.util;
+
+/*
+* Class that gather utils functions to format citations urls
+*/
+public class CitationsUtils {
+	
+	//Change citation url from old to new format (return parameter)
+	public static String formatCitationUrl(String oldUrlCitations) {
+		return oldUrlCitations + "_stub";
+	}
+	
+}
