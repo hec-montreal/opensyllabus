f0545224546677bad773b101bea135b93966f76c
Curtis van Osch <curtis.van-osch@hec.ca>
2013-04-15 20:43:50 +0000

    ZCII-390 : prendre l'url de la config et inserer le mot AND

--- osyl-common/osyl-common-shared/src/java/org/sakaiquebec/opensyllabus/shared/util/CitationsUtils.java
+++ osyl-common/osyl-common-shared/src/java/org/sakaiquebec/opensyllabus/shared/util/CitationsUtils.java
@@ -1,13 +1,25 @@
 package org.sakaiquebec.opensyllabus.shared.util;
 
+import org.sakaiproject.citation.api.ConfigurationService;
+import org.sakaiproject.component.cover.ComponentManager;
+
 /*
 * Class that gather utils functions to format citations urls
 */
 public class CitationsUtils {
+	private static ConfigurationService configService = 
+			(ConfigurationService) ComponentManager.get(ConfigurationService.class);
 	
 	//Change citation url from old to new format (return parameter)
-	public static String formatCitationUrl(String oldUrlCitations) {
-		return oldUrlCitations + "_stub";
+	public static String formatCitationUrl(String oldCitationUrl) {
+		
+		if (oldCitationUrl.startsWith("http://taos.hec.ca")) {
+			String query = oldCitationUrl.substring(oldCitationUrl.lastIndexOf('=')+1);
+			
+			return configService.getSiteConfigLibraryUrlResolverAddress() + 
+					query.replaceAll(" ", " AND ");
+		}
+		else 
+			return oldCitationUrl;
 	}
-	
 }
