afc1cf18c85f357c16c921c1498e0782d53aefad
Christophe Henno <christophe.henno@hec.ca>
2013-11-21 16:42:35 +0000

    ZCII-876 - Modifier la synchro pour intégrer les données calendrier

--- osyl-admin/osyl-admin-server-api/api/src/java/org/sakaiquebec/opensyllabus/admin/cmjob/api/CourseEventSynchroJob.java
+++ osyl-admin/osyl-admin-server-api/api/src/java/org/sakaiquebec/opensyllabus/admin/cmjob/api/CourseEventSynchroJob.java
@@ -1,8 +1,11 @@
 package org.sakaiquebec.opensyllabus.admin.cmjob.api;
 
+import java.io.IOException;
+
 /**
  * Job de synchro du fichier d'extract contenant les événements de cours avec la
  * table HEC_EVENT
+ *
  * @author 11183065
  *
  */
@@ -10,8 +13,14 @@ public interface CourseEventSynchroJob {
 
 	/**
 	 * execute la job
-	 * @param filePath chemin du fichier d'extract contenant les événements de cours
+	 *
+	 * @param filePath
+	 *            chemin du fichier d'extract contenant les événements de cours
+	 * @throws IOException
+	 *             En cas de problème lors de la lecture du fichier
+	 * @throws InvalidStateException
+	 * 			Dans la cas ou la colonne state d'une ligne de la table hec_event n'est pas nulle
 	 */
-	void execute(String filePath);
+	void execute(String filePath) throws IOException, InvalidStateException;
 
 }
