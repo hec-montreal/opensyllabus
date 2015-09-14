255b509cfa657241696d0f9fe7abfa1ee8e038ae
Curtis van Osch <curtis.van-osch@hec.ca>
2015-09-14 14:18:03 +0000

    Rajouter le code supprimer pour la mise en prod (ZCII-1896)

--- osyl-admin/osyl-admin-sakai-impl/impl/src/java/org/sakaiquebec/opensyllabus/admin/impl/extracts/GenericProfCoursMapFactory.java
+++ osyl-admin/osyl-admin-sakai-impl/impl/src/java/org/sakaiquebec/opensyllabus/admin/impl/extracts/GenericProfCoursMapFactory.java
@@ -8,140 +8,58 @@ import org.apache.commons.logging.LogFactory;
 
 public class GenericProfCoursMapFactory {
 
-    private static Log log = LogFactory
-	    .getLog(GenericProfCoursMapFactory.class);
+	private static Log log = LogFactory.getLog(GenericProfCoursMapFactory.class);
 
-    public static ProfCoursMap buildMap(String completeFileName,
-	    DetailCoursMap detailCoursMap, DetailSessionsMap detailSessionsMap)
-	    throws java.io.IOException {
+	public static Map<String, List<ProfCoursMapEntry>> buildMap(String completeFileName)
+			throws java.io.IOException {
 
-	// Pour faire reference aux cours de chaque prof, on a besoin du details
-	// des cours... S'il n'est pas disponible, on ne peut pas continuer
-	if (detailCoursMap == null) {
-	    throw new IllegalStateException(
-		    "Erreur: la structure DetailCoursMap doit etre initialisee "
-			    + "avant ProfCoursMap");
-	}
+		Map<String, List<ProfCoursMapEntry>> map = new HashMap<String, List<ProfCoursMapEntry>>();
 
-	ProfCoursMap map;
-	try {
-	    map = getInstance(completeFileName);
-	    print("Mise a jour de la map...");
-	} catch (FileNotFoundException e) {
-	    print("La map n'a pas ete trouvee, on la recree au complet.");
-	    map = new ProfCoursMap();
-	} catch (IOException e) {
-	    print("buildMap: exception dans getInstance: " + e);
-	    throw e;
-	}
+		BufferedReader breader =
+				new BufferedReader(new InputStreamReader(new FileInputStream(
+						completeFileName), "ISO-8859-1"));
 
-	// Voir le commentaire de methode ci-dessus
-	// removeAllCours(map, detailCoursMap,
-	// detailSessionsMap.getLatestSession());
+		String buffer;
+		String[] token;
 
-	BufferedReader breader =
-		new BufferedReader(new InputStreamReader(new FileInputStream(
-			completeFileName), "ISO-8859-1"));
-	String buffer;
-	String[] token;
-	int i;
-	// We remove the first line containing the title
-	breader.readLine();
+		// We remove the first line containing the title
+		breader.readLine();
 
-	// fait le tour des lignes du fichier
-	while ((buffer = breader.readLine()) != null) {
-	    token = buffer.split(";");
-	    i = 0;
-	    ProfCoursMapEntry entry;
-	    String emplId = token[i++];
-	    String catalogNbr = token[i++];
-	    String strm = token[i++];
-	    String sessionCode = token[i++];
-	    String section = token[i++];
-	    String acadOrg = token[i++];
-	    String role = token[i++];
-	    String strmId = strm + sessionCode;
+		// fait le tour des lignes du fichier
+		while ((buffer = breader.readLine()) != null) {
+			token = buffer.split(";");
 
-	    if (catalogNbr != null)
-		catalogNbr = catalogNbr.trim();
-	    if (map.containsKey(emplId)) {
-		entry = map.get(emplId);
-	    } else {
-		entry = new ProfCoursMapEntry(emplId);
-		map.put(entry);
-	    }
+			String emplId = token[0];
+			String catalogNbr = token[1];
+			String strm = token[2];
+			String sessionCode = token[3];
+			String classSection = token[4];
+//			Skip acadorg et strmId, les lignes ont parfois trop de colonnes! donc elles ne sont pas fiable.
+//			String acadOrg = token[5];
+			String role = token[6];
+//			String strmId = token[7];
 
-	    // TODO: removed for the purpose of the tests, put back when done
-	    // if (cours == null) {
-	    // // throw new IllegalStateException("cours == null pour " +
-	    // // buffer);
-	    // }
+			// terrible hack because the number of columns per line is not consistent
+			if (!"Enseignant".equals(role))
+				role = "Coordonnateur";
 
-	    DetailCoursMapEntry cours =
-		    detailCoursMap.get(catalogNbr, strmId, section);
+			if (catalogNbr != null) {
+				catalogNbr = catalogNbr.trim();
+			}
 
-	    entry.setAcadOrg(acadOrg);
-	    entry.setRole(role);
-	    entry.setStrmId(strmId);
-	    if (cours != null && "Enseignant".equalsIgnoreCase(role.trim())) {
-		// On ajoute le cours a cet prof uniquement s'il ne l'a pas
-		// deja
-		// (ce qui est le cas pour tous ses cours d'anciennes sessions).
-		if (!entry.containsCours(cours)) {
-		    entry.addCours(cours);
+			List<ProfCoursMapEntry> listProfs;
+			String key = catalogNbr + strm + sessionCode + classSection;
+			if (map.containsKey(key)) {
+				listProfs = map.get(key);
+			} else {
+				listProfs = new ArrayList<ProfCoursMapEntry>();
+				map.put(key, listProfs);
+			}
+			listProfs.add(new ProfCoursMapEntry(emplId, catalogNbr, strm, sessionCode, classSection, null, role, null));
 		}
 
-		// et la contrepartie dans le cours...
-		if (!cours.containsProfesseur(entry)) {
-		    cours.addProfesseur(entry);
-		}
+		breader.close();
 
-	    }
-
-	    // Si c'est un coordonnateur on l'ajoute comme tel dans le cours
-	    if (!entry.isEnseignant() && cours != null) {
-		cours.setCoordonnateur(entry);
-	    }
-	}
-
-	// ferme le tampon
-	breader.close();
-
-	return map;
-    } // buildMap
-
-    /**
-     * Enleve tous les cours correspondant a la session specifiee dans chaque
-     * prof. L'operation complementaire, c'est a dire enlever les profs des
-     * cours correspondants est aussi effectuee afin de maintenir l'integrite.
-     */
-    private static void removeAllCours(ProfCoursMap map,
-	    DetailCoursMap detailCoursMap, DetailSessionsMapEntry latestSession) {
-	Iterator<ProfCoursMapEntry> profs = map.values().iterator();
-	ProfCoursMapEntry prof;
-	while (profs.hasNext()) {
-	    prof = (ProfCoursMapEntry) profs.next();
-	    prof.removeAllCours(latestSession);
-	}
-
-	Iterator<DetailCoursMapEntry> coursIterator =
-		detailCoursMap.values().iterator();
-	DetailCoursMapEntry cours;
-	while (coursIterator.hasNext()) {
-	    cours = (DetailCoursMapEntry) coursIterator.next();
-	    if (cours.isInSession(latestSession)) {
-		cours.removeAllProfs();
-	    }
-	}
-    } // removeAllCours
-
-    public static ProfCoursMap getInstance(String completeFileName)
-	    throws IOException {
-
-	return new ProfCoursMap();
-    }
-
-    protected static void print(String msg) {
-	log.info("GenericProfCoursMapFactory: " + msg);
-    }
+		return map;
+	} // buildMap
 }
--- osyl-admin/osyl-admin-sakai-impl/impl/src/java/org/sakaiquebec/opensyllabus/admin/impl/extracts/ProfCoursMapEntry.java
+++ osyl-admin/osyl-admin-sakai-impl/impl/src/java/org/sakaiquebec/opensyllabus/admin/impl/extracts/ProfCoursMapEntry.java
@@ -2,253 +2,28 @@ package org.sakaiquebec.opensyllabus.admin.impl.extracts;
 
 import java.util.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
  * Represente les donnees provenant de l'extract prof_cours.dat<br/>
  * <br/>
- * Chaque <code>ProfCoursEntry</code> correspond a un professeur et permet
- * d'acceder aux cours qu'il donne.
  */
-public class ProfCoursMapEntry implements java.io.Serializable {
+@AllArgsConstructor
+@Data
+public class ProfCoursMapEntry {
 
-    // Pour assurer la compatibilite des instances serialisees meme
-    // quand on change la classe...
-    public static final long serialVersionUID = -913935703184167796L;
-	
-    public static final String ENSEIGNANT = "Enseignant";
-    
-    public static final String COORDONNATEUR = "Coordonnateur";
+	private static Log log = LogFactory.getLog(ProfCoursMapEntry.class);
 
-
-    private static Log log = LogFactory.getLog(ProfCoursMapEntry.class);
-
-    private Vector<DetailCoursMapEntry> cours;
-    private Map<String, DetailCoursMapEntry> coursCoordonnes;
     private String emplId;
-    private String descLong ;
-    private String nDescLong;
+    private String catalogNbr;
+    private String strm;
+    private String sessionCode;
+    private String classSection;
     private String acadOrg;
     private String role;
     private String strmId;
-
-    public String getDescLong() {
-        return descLong;
-    }
-
-    public void setDescLong(String descLong) {
-        this.descLong = descLong;
-    }
-
-    public String getNDescLong() {
-        return nDescLong;
-    }
-
-    public void setNDescLong(String descLong) {
-        nDescLong = descLong;
-    }
-
-    public String getAcadOrg() {
-        return acadOrg;
-    }
-
-    public void setAcadOrg(String acadOrg) {
-        this.acadOrg = acadOrg;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public boolean isEnseignant (){
-	if (role == null)
-	    return false;
-	else 
-	    return ENSEIGNANT.equalsIgnoreCase(role);
-    }
-    
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getStrmId() {
-        return strmId;
-    }
-
-    public void setStrmId(String strmId) {
-        this.strmId = strmId;
-    }
-
-    public void setCours(Vector<DetailCoursMapEntry> cours) {
-        this.cours = cours;
-    }
-
-    public void setCoursCoordonnes(Map<String, DetailCoursMapEntry> coursCoordonnes) {
-        this.coursCoordonnes = coursCoordonnes;
-    }
-
-    public void setEmplId(String emplId) {
-        this.emplId = emplId;
-    }
-
-    /**
-     * One arg Constructor.
-     */
-    ProfCoursMapEntry(String emplId) {
-	this.emplId = emplId;
-	cours = new Vector<DetailCoursMapEntry>();
-	coursCoordonnes = new HashMap<String, DetailCoursMapEntry>();
-    }
-
-    public String getEmplId() {
-	return emplId;
-    }
-
-    // ============= Gestion des cours enseignes =============
-    //
-    public Iterator<DetailCoursMapEntry> getCours() {
-	return getCoursVector().iterator();
-    }
-
-    Vector<DetailCoursMapEntry> getCoursVector() {
-	return cours;
-    }
-
-    public DetailCoursMapEntry getCours(int i) {
-	return (DetailCoursMapEntry) getCoursVector().get(i);
-    }
-
-    public int getCoursCount() {
-	return getCoursVector().size();
-    }
-
-    /**
-     * Note: ne pas faire de modification a la liste des cours d'un prof sans
-     * faire l'equivalent dans la liste des profs d'un cours!
-     */
-    public void addCours(DetailCoursMapEntry cours) {
-	if (!containsCours(cours)) {
-	    getCoursVector().add(cours);
-	}
-    }
-
-    public boolean containsCours(DetailCoursMapEntry cours) {
-	return getCoursVector().contains(cours);
-    }
-
-    /**
-     * Supprime tous les cours de ce prof, donnes ou coordonnes dans la session
-     * specifiee.<br>
-     * <br>
-     * Note: ne pas faire de modification a la liste des cours d'un prof sans
-     * faire l'equivalent dans la liste des profs d'un cours!
-     */
-    public void removeAllCours(DetailSessionsMapEntry session) {
-	Iterator<DetailCoursMapEntry> coursIterator = getCours();
-	DetailCoursMapEntry cours;
-	while (coursIterator.hasNext()) {
-	    cours = (DetailCoursMapEntry) coursIterator.next();
-	    if (cours.isInSession(session)) {
-		// Attention il ne faut pas faire getCoursVector().remove(cours)
-		// car ca cree une java.util.ConcurrentModificationException.
-		coursIterator.remove();
-	    }
-	}
-	// TODO: ce code est rigoureusement identique a celui du dessus et a
-	// celui utilise dans EtudiantCoursMapEntry... il faudrait le partager!
-	coursIterator = getCoursCoordonnes();
-	while (coursIterator.hasNext()) {
-	    cours = (DetailCoursMapEntry) coursIterator.next();
-	    if (cours.isInSession(session)) {
-		// Attention il ne faut pas faire getCoursVector().remove(cours)
-		// car ca cree une java.util.ConcurrentModificationException.
-		coursIterator.remove();
-	    }
-	}
-    } // removeAllCours
-
-    // private void removeCours(DetailCoursMapEntry cours) {
-    // getCoursVector().remove(cours);
-    // }
-
-    public boolean containsAnyGroupeCoursOf(DetailCoursMapEntry cours) {
-	Iterator<DetailCoursMapEntry> iterator = getCours();
-	while (iterator.hasNext()) {
-	    if (cours.isSameCours((DetailCoursMapEntry) iterator.next())) {
-		return true;
-	    }
-	}
-	return false;
-    } // containsAnyGroupeCoursOf
-
-    // ==== Gestion des cours uniquement coordonnes (ie: non enseignes!) =====
-    //
-    public Iterator<DetailCoursMapEntry> getCoursCoordonnes() {
-	return getCoursCoordonnesMap().values().iterator();
-    }
-
-    private Map<String, DetailCoursMapEntry> getCoursCoordonnesMap() {
-	return coursCoordonnes;
-    }
-
-    Collection<DetailCoursMapEntry> getCoursCoordonnesCollection() {
-	return coursCoordonnes.values();
-    }
-
-    public int getCoursCoordonnesCount() {
-	return getCoursCoordonnesMap().size();
-    }
-
-    public void addCoursCoordonne(DetailCoursMapEntry cours) {
-	getCoursCoordonnesMap().put(cours.getStrm() + cours.getCatalogNbr(),
-		cours);
-    } // addCoursCoordonne
-
-    public boolean containsCoursCoordonne(DetailCoursMapEntry cours) {
-	return getCoursCoordonnesMap().containsKey(
-		cours.getStrm() + cours.getCatalogNbr());
-    }
-
-    public String toString() {
-	return "Professeur " + getEmplId() + " coordonne: "
-		+ getCoursCoordonnesCollection().size() + " cours : "
-		+ getCoursCoordonnesMap().keySet();
-    }
-
-    /**
-     * On redefinit <code>hashCode()</code> pour assurer que malgre la
-     * serialisation 2 instances soit reconnues egales lorsque c'est approprie
-     * (meme emplId).
-     */
-    public int hashCode() {
-	try {
-	    // On ajoute le chiffre 1 devant car certains matricules commencent
-	    // par un ou plusieurs zeros.
-	    return Integer.parseInt("1" + getEmplId());
-	} catch (NumberFormatException e) {
-	    log.info("ProfCoursMapEntry.hashCode: " + e);
-	    return super.hashCode();
-	}
-    } // hashCode
-
-    /**
-     * On redefinit <code>equals(Object)</code> pour assurer que malgre la
-     * serialisation 2 instances soit reconnues egales lorsque c'est approprie.
-     */
-    public boolean equals(Object o) {
-	if (o == null) {
-	    return false;
-	} else if (!o.getClass().getName().equals(
-		"ca.hec.peoplesoft.ProfCoursMapEntry")) {
-	    return false;
-	} else {
-	    if (o.hashCode() == hashCode()) {
-		return true;
-	    } else {
-		return false;
-	    }
-	}
-    } // equals
-
 }
