40072d968df4b992fceb3b39bd5351898f06c4c4
Curtis van Osch <curtis.van-osch@hec.ca>
2015-12-07 22:19:54 +0000

    ZCII-2053 : fix bug quand une citation contient plusieurs fois le meme attribut

--- osyl-editor/osyl-editor-gwt/src/main/java/org/sakaiquebec/opensyllabus/client/remoteservice/json/callback/OsylCitationRemoteDirectoryContentCallBackAdaptator.java
+++ osyl-editor/osyl-editor-gwt/src/main/java/org/sakaiquebec/opensyllabus/client/remoteservice/json/callback/OsylCitationRemoteDirectoryContentCallBackAdaptator.java
@@ -30,14 +30,14 @@ public class OsylCitationRemoteDirectoryContentCallBackAdaptator extends
 	//TODO: get this from settings?
     protected final static List<String> SUPPORTED_CITATION_TYPES =
 	    Arrays.asList(
-	    		new String[] { 
+	    		new String[] {
 	    				CitationSchema.TYPE_ARTICLE,
 	    				CitationSchema.TYPE_BOOK,
 	    				CitationSchema.TYPE_CHAPTER,
 	    				CitationSchema.TYPE_ELECTRONIC,
 	    				CitationSchema.TYPE_REPORT,
 	    				CitationSchema.TYPE_THESIS,
-	    				CitationSchema.TYPE_UNKNOWN, 
+	    				CitationSchema.TYPE_UNKNOWN,
 	    				CitationSchema.TYPE_PROCEED });
 
     /**
@@ -76,7 +76,7 @@ public class OsylCitationRemoteDirectoryContentCallBackAdaptator extends
 	JSONString lastModified =
 		(JSONString) jObject.get("DAV:getlastmodified");
 	JSONObject citations = (JSONObject) jObject.get("citations");
-	
+
 	Set<String> keys = citations.keySet();
 	for (Iterator<String> iter = keys.iterator(); iter.hasNext();) {
 	    String citationId = (String) iter.next();
@@ -84,117 +84,100 @@ public class OsylCitationRemoteDirectoryContentCallBackAdaptator extends
 	    JSONObject properties = (JSONObject) jsCitation.get("properties");
 
 	    JSONString mediaType =
-		    (JSONString) properties.get("sakai:mediatype");
+	    		getPropertyValue(properties, "sakai:mediatype");
+
 	    if (SUPPORTED_CITATION_TYPES.contains(mediaType.stringValue())) {
-		JSONString title =
-			(JSONString) properties.get(CitationSchema.TITLE);
-		JSONString isn =
-			(JSONString) properties.get(CitationSchema.ISN);
-		JSONString volume =
-				(JSONString) properties.get(CitationSchema.VOLUME);
-		JSONString edition =
-				(JSONString) properties.get(CitationSchema.EDITION);
-		JSONString issue =
-			(JSONString) properties.get(CitationSchema.ISSUE);
-		JSONString pages =
-			(JSONString) properties.get(CitationSchema.PAGES);
-		JSONString publisher =
-			(JSONString) properties.get(CitationSchema.PUBLISHER);
-		JSONString year =
-			(JSONString) properties.get(CitationSchema.YEAR);
-		JSONString date =
-			(JSONString) properties.get(CitationSchema.DATE);
-		JSONString doi =
-			(JSONString) properties.get(CitationSchema.DOI);
-		JSONString publicationLocation =
-			(JSONString) properties
-				.get(CitationSchema.PUBLICATION_LOCATION);
-		JSONString sourceTitle =
-			(JSONString) properties
-				.get(CitationSchema.SOURCE_TITLE);
-		JSONString start_page =
-				(JSONString) properties.get(CitationSchema.START_PAGE);
-		JSONString end_page =
-				(JSONString) properties.get(CitationSchema.END_PAGE);
-		JSONString date_retrieved =
-				(JSONString) properties.get(CitationSchema.DATE_RETRIEVED);
-		JSONString document_type =
-				(JSONString) properties.get(CitationSchema.DOCUMENT_TYPE);
-		JSONString university =
-				(JSONString) properties.get(CitationSchema.UNIVERSITY);
+	    	JSONString title =
+	    			getPropertyValue(properties, CitationSchema.TITLE);
+	    	JSONString isn =
+	    			getPropertyValue(properties, CitationSchema.ISN);
+	    	JSONString volume =
+	    			getPropertyValue(properties, CitationSchema.VOLUME);
+	    	JSONString edition =
+	    			getPropertyValue(properties, CitationSchema.EDITION);
+	    	JSONString issue =
+	    			getPropertyValue(properties, CitationSchema.ISSUE);
+	    	JSONString pages =
+	    			getPropertyValue(properties, CitationSchema.PAGES);
+	    	JSONString publisher =
+	    			getPropertyValue(properties, CitationSchema.PUBLISHER);
+	    	JSONString year =
+	    			getPropertyValue(properties, CitationSchema.YEAR);
+	    	JSONString date =
+	    			getPropertyValue(properties, CitationSchema.DATE);
+	    	JSONString doi =
+	    			getPropertyValue(properties, CitationSchema.DOI);
+	    	JSONString publicationLocation =
+	    			getPropertyValue(properties, CitationSchema.PUBLICATION_LOCATION);
+	    	JSONString sourceTitle =
+	    			getPropertyValue(properties, CitationSchema.SOURCE_TITLE);
+	    	JSONString start_page =
+	    			getPropertyValue(properties, CitationSchema.START_PAGE);
+	    	JSONString end_page =
+	    			getPropertyValue(properties, CitationSchema.END_PAGE);
+	    	JSONString date_retrieved =
+	    			getPropertyValue(properties, CitationSchema.DATE_RETRIEVED);
+	    	JSONString document_type =
+	    			getPropertyValue(properties, CitationSchema.DOCUMENT_TYPE);
+	    	JSONString university =
+	    			getPropertyValue(properties, CitationSchema.UNIVERSITY);
+	    	JSONString resourceType =
+	    			getPropertyValue(properties, CitationSchema.CITATION_RESOURCE_TYPE);
 
-		//Type of resource
-		JSONValue resourceTypeValue =
-			(JSONValue) properties
-				.get(CitationSchema.CITATION_RESOURCE_TYPE);
+	    	JSONString identifierTypeUrlLabel =
+    			getPropertyValue(properties, PREFERRED_URL_LABEL);
 
-		JSONString resourceType = null;
-		if (resourceTypeValue != null) {
-			JSONArray typeArray = resourceTypeValue.isArray();
+	    	JSONString identifierTypeUrl =
+	    			getPropertyValue(properties, PREFERRED_URL);
 
-			if (typeArray != null) {
-				// use only the first asmResourceType
-				resourceType = (JSONString) typeArray.get(0);
-			} else {
-				resourceType = (JSONString) resourceTypeValue;
-			}
-		}
+	    	JSONString identifierTypeLibrary =
+	    			getPropertyValue(properties, URL);
 
+	    	JSONString identifierTypeBookstore =
+	    			getPropertyValue(properties, BOOKSTORE_URL);
 
-		JSONString identifierTypeUrlLabel =
-		    (JSONString) properties.get(PREFERRED_URL_LABEL);
-		
-		JSONString identifierTypeUrl =
-			(JSONString) properties.get(PREFERRED_URL);
-		JSONString identifierTypeLibrary =
-			(JSONString) properties
-				.get(URL);
+	    	JSONString identifierTypeNoUrl =
+	    			getPropertyValue(properties, NO_URL);
 
-		JSONString identifierTypeBookstore =
-			(JSONString) properties.get(BOOKSTORE_URL);
-
-		JSONString identifierTypeNoUrl =
-			(JSONString) properties.get(NO_URL);
-
-		List<String> creatorsList = new ArrayList<String>();
-		JSONValue creatorValue = properties.get("creator");
-		String creatorsString = "";
-		if (creatorValue != null) {
-		    JSONArray creatorsArrays = (JSONArray) creatorValue;
-		    for (int i = 0; i < creatorsArrays.size(); i++) {
-			String creatorString =
+	    	List<String> creatorsList = new ArrayList<String>();
+	    	JSONValue creatorValue = properties.get("creator");
+	    	String creatorsString = "";
+	    	if (creatorValue != null) {
+	    		JSONArray creatorsArrays = (JSONArray) creatorValue;
+	    		for (int i = 0; i < creatorsArrays.size(); i++) {
+	    			String creatorString =
 				((JSONString) creatorsArrays.get(i))
 					.stringValue();
-			creatorsList.add(creatorString);
-			creatorsString += creatorString + " & ";
-		    }
+	    			creatorsList.add(creatorString);
+	    			creatorsString += creatorString + " & ";
+	    		}
 		    if (!creatorsString.equals(""))
-			creatorsString =
+	    			creatorsString =
 				creatorsString.substring(0, creatorsString
 					.length() - 3);
-		}
+	    	}
 
-		List<String> editorsList = new ArrayList<String>();
-		JSONValue editorValue = properties.get("editor");
-		String editorsString = "";
-		if (editorValue != null) {
-		    if (editorValue.isArray() == null) {
-			editorsString = ((JSONString) editorValue).toString();
-		    } else {
-			JSONArray editorsArrays = (JSONArray) editorValue;
-			for (int i = 0; i < editorsArrays.size(); i++) {
-			    String editorString =
+	    	List<String> editorsList = new ArrayList<String>();
+	    	JSONValue editorValue = properties.get("editor");
+	    	String editorsString = "";
+	    	if (editorValue != null) {
+	    		if (editorValue.isArray() == null) {
+	    			editorsString = ((JSONString) editorValue).toString();
+	    		} else {
+	    			JSONArray editorsArrays = (JSONArray) editorValue;
+	    			for (int i = 0; i < editorsArrays.size(); i++) {
+	    				String editorString =
 				    ((JSONString) editorsArrays.get(i))
 					    .stringValue();
-			    editorsList.add(editorString);
-			    editorsString += editorString + ", ";
-			}
+	    				editorsList.add(editorString);
+	    				editorsString += editorString + ", ";
+	    			}
 			if (!editorsString.equals(""))
-			    editorsString =
+	    				editorsString =
 				    editorsString.substring(0, editorsString
 					    .length() - 2);
-		    }
-		}
+	    		}
+	    	}
 
 		OsylCitationItem csi = new OsylCitationItem();
 		csi.setId(citationId);
@@ -235,7 +218,7 @@ public class OsylCitationRemoteDirectoryContentCallBackAdaptator extends
 				.stringValue());
 		csi.setProperty(CitationSchema.UNIVERSITY, university == null ? "" : university
 				.stringValue());
-		
+
 		//Type of resource
 		csi.setProperty(CitationSchema.CITATION_RESOURCE_TYPE,
 				resourceType == null ? "" : resourceType.stringValue());
@@ -248,7 +231,7 @@ public class OsylCitationRemoteDirectoryContentCallBackAdaptator extends
 			COPropertiesType.IDENTIFIER_TYPE_OTHERLINK,
 			identifierTypeUrl == null ? "" : identifierTypeUrl
 				.stringValue());
-		
+
 		csi.getCOProperty(COPropertiesType.IDENTIFIER,
 			COPropertiesType.IDENTIFIER_TYPE_OTHERLINK)
 			.addAttribute(
@@ -301,5 +284,22 @@ public class OsylCitationRemoteDirectoryContentCallBackAdaptator extends
 
 	return osylCitationListItem;
 
+    }
+
+    // return a single value for this property
+    // (selecting the first if there are many, to prevent the tool failing if that's the case).
+    private JSONString getPropertyValue(JSONObject properties, String propertyName) {
+    	JSONValue propertyValue = (JSONValue) properties.get(propertyName);
+
+    	if (propertyValue != null) {
+    		JSONArray array = propertyValue.isArray();
+
+    		if (array != null) {
+    			return (JSONString) array.get(0);
+    		} else {
+    			return (JSONString) propertyValue;
+    		}
+    	}
+    	return null;
     }
 }
