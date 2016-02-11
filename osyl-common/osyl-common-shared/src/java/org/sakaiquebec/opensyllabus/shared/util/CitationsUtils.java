6001e7b8ca27b78e8648b438e78728dac46f97d0
Curtis van Osch <curtis.van-osch@hec.ca>
2016-02-11 16:12:34 +0000

    ZCII-2130 : ajouter la possibilité de spécifier une liste d'url dans le sakai.properties qui sont ensuite remplacé dans le plan de cours si on les vois (lien vers la bibliothèque)

--- osyl-common/osyl-common-shared/src/java/org/sakaiquebec/opensyllabus/shared/util/CitationsUtils.java
+++ osyl-common/osyl-common-shared/src/java/org/sakaiquebec/opensyllabus/shared/util/CitationsUtils.java
@@ -2,6 +2,7 @@ package org.sakaiquebec.opensyllabus.shared.util;
 
 import org.sakaiproject.citation.api.ConfigurationService;
 import org.sakaiproject.component.cover.ComponentManager;
+import org.sakaiproject.component.cover.ServerConfigurationService;
 
 /*
 * Class that gather utils functions to format citations urls
@@ -9,6 +10,10 @@ import org.sakaiproject.component.cover.ComponentManager;
 public class CitationsUtils {
 	private static ConfigurationService configService = 
 			(ConfigurationService) ComponentManager.get(ConfigurationService.class);
+
+	// ne charger qu'une seule fois la liste
+	private static final String[] openUrlAddressesToReplace = 
+			ServerConfigurationService.getStrings("osyl.replace.openurl.address");
 	
 	//Change citation url from old to new format (return parameter)
 	public static String formatCitationUrl(String oldCitationUrl) {
@@ -19,7 +24,18 @@ public class CitationsUtils {
 			return configService.getSiteConfigLibraryUrlResolverAddress() + 
 					query.replaceAll(" ", " AND ");
 		}
-		else 
-			return oldCitationUrl;
+		// ZCII-2002 : permettre de spécifier une liste d'url dans sakai.properties
+		//             qui devraient être remplacé par celui qui est dans la config
+		//             lorsqu'on charge un plan de cours
+		if (null != openUrlAddressesToReplace) {
+			for (String url : openUrlAddressesToReplace) {
+				if (oldCitationUrl.startsWith(url)) {
+					String query = oldCitationUrl.substring(oldCitationUrl.indexOf('?'));		
+					return configService.getSiteConfigOpenUrlResolverAddress() + query;					
+				}
+			}
+		}
+		
+		return oldCitationUrl;
 	}
 }
